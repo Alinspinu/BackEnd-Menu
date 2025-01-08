a7038d5754a304921560819d56bc2433fc83062e
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-08 16:35:17 +0200

    all done

--- models/office/imp-sheet.js
+++ models/office/imp-sheet.js
@@ -32,10 +32,12 @@ const imparimentSheetSchema = new Schema({
 
 
 imparimentSheetSchema.pre('deleteOne', { document: true }, async function (next) {
+    console.log('hit the delete one pre')
         try{
             const dbSheet = await this.model.findOne(this.getQuery())
                     .populate({path: 'ings.ing', select: 'productIngredient ings name price um tva'})
                 if(dbSheet){
+                    console.log(dbSheet)
                 const ingsPromises = dbSheet.ings.flatMap(ing => {
                     if(ing.ing.productIngredient){
                     return ing.ing.ings.map(ingg =>
