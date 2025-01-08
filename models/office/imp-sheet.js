c9c5481dc4975eb906a6893d2e41afc35961d8bc
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-08 17:02:39 +0200

    all done

--- models/office/imp-sheet.js
+++ models/office/imp-sheet.js
@@ -32,7 +32,7 @@ const imparimentSheetSchema = new Schema({
 const ImpSheet = mongoose.model('ImpSheet', imparimentSheetSchema);
 
 imparimentSheetSchema.pre('deleteOne', { document: true, query: false }, async function (next) {
-    console.warn('hit the delete one pre')
+    console.log('hit the delete one pre')
         try{
             const query = {_id: this._id}
             const dbSheet = await ImpSheet.findOne(query)
