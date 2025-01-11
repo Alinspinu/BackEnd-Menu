d61f7f846a789d4c926945bc64b75e7dec53ef0b
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-11 13:31:08 +0200

    all done

--- controlers/back-office/orders.js
+++ controlers/back-office/orders.js
@@ -35,23 +35,15 @@ module.exports.getOrder = async (req, res, next) => {
     if(day && !end && !start) {
         const start = new Date(day).setHours(0,0,0,0)
         const end = new Date(day).setHours(23,59,59,9999)
-        const orders = await Order.find({ locatie: loc , createdAt: {$gte: start, $lt: end}, status: 'done'})
+        const orders = await Order.find({ locatie: loc , createdAt: {$gte: start, $lt: end}, status: 'done'}).populate({path: 'masaRest', select: 'name index'})
         const openOrders = await Order.find({ locatie: loc, status: 'open'}).populate({path: 'masaRest', select: 'name index'})
         const delProds = await DelProd.find({locatie: loc, createdAt: {$gte: start, $lt: end}})
         res.status(200).json({orders: [...orders, ...openOrders], delProducts: delProds})
     }
     if(!day && !end && !start) {
         const today = new Date(Date.now()).setHours(0,0,0,0)
-        const orders = await Order.find({ locatie: loc , createdAt: {$gte: today}, status: 'done'})
-        const openOrders = await Order.find({ locatie: loc, status: 'open'})
-        for(let order of openOrders) {
-            const table = await Table.findOne({index: order.masa})
-            if(table){
-                order.masaRest = table._id
-                await order.save()
-                console.log('order nr ', order.index, ' saved!')
-            }
-        }
+        const orders = await Order.find({ locatie: loc , createdAt: {$gte: today}, status: 'done'}).populate({path: 'masaRest', select: 'name index'})
+        const openOrders = await Order.find({ locatie: loc, status: 'open'}).populate({path: 'masaRest', select: 'name index'})
         const delProds = await DelProd.find({locatie: loc, createdAt: {$gte: today}})
         res.status(200).json({orders: [...orders, ...openOrders], delProducts: delProds})
     }
@@ -60,6 +52,15 @@ module.exports.getOrder = async (req, res, next) => {
         console.log(err)
     }
 }
+
+// for(let order of openOrders) {
+//     const table = await Table.findOne({index: order.masa})
+    // if(table){
+    //     order.masaRest = table._id
+    //     await order.save()
+    //     console.log('order nr ', order.index, ' saved!')
+    // }
+// }
 
 module.exports.calcDep = async (req, res, next) => {
     const {start, end, loc} = req.query
