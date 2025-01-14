0815b834db059e01ad0980dfbd0fc1ef3f98b001
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-14 16:04:51 +0200

    all done

--- controlers/users/users.js
+++ controlers/users/users.js
@@ -236,7 +236,10 @@ module.exports.updateVivaData = async (req, res, next) => {
 module.exports.deletePaymentEntry = async (req, res) => {
     try{
         const {payID, userID} = req.query
+        console.log(userID, payID)
         const user = await User.findByIdAndUpdate(userID, { $pull: { 'employee.payments': payID }}, {new: true})
+        console.log(user.name)
+        console.log(user.payments)
         res.status(200).json({message: 'Logul a fost stres cu success!'})
     } catch(error){
         console.log(error)
