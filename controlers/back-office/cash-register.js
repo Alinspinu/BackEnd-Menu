0d7cbdb3105bd88da2897de5fd166ff6620a5ec0
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-14 15:15:21 +0200

    all done

--- controlers/back-office/cash-register.js
+++ controlers/back-office/cash-register.js
@@ -104,7 +104,7 @@ module.exports.addEntry = async (req, res, next) => {
             const payment = {
                 amount: amount,
                 tip: typeOf,
-                date: new Date('2024-12-31'),
+                date: entryDate,
                 workMonth: month
             }
            await User.findOneAndUpdate({_id: user[0]}, {$push: {'employee.payments': payment}})
