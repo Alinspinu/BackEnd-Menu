3dc31e8e4b5cafbba710676e87e503170f000baf
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-15 15:59:54 +0200

    all done

--- cloudinary/photo-true.js
+++ cloudinary/photo-true.js
@@ -1,6 +1,8 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const crypto = require('crypto');
+const {generateSoketId} = require('../utils/functions')
+
 
 function generateSignature(paramsToSign) {
     const cloudinarySecret = process.env.CLOUDINARY_SECRET;
@@ -21,42 +23,43 @@ cloudinary.config({
 });
 
 
-const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
-    params: async (req, file) => {
-        const timestamp = Math.floor(Date.now() / 1000); // Current Unix timestamp
-        const paramsToSign = {
-            folder: 'uploads', // The folder name
-            timestamp: timestamp,
-            transformation: [{ width: 555, height: 888, crop: "fill" }]
-        };
-        const signature = generateSignature(paramsToSign);
-
-        return {
-            folder: 'uploads',
-            transformation: [{ width: 555, height: 888, crop: "fill" }],
-            allowed_formats: ['jpeg', 'png', 'jpg', 'mp4'],
-            resource_type: 'auto',
-            api_key: process.env.CLOUDINARY_KEY,
-            timestamp: timestamp,
-            signature: signature
-        };
-    }
-});
-
-
-
 // const storage = new CloudinaryStorage({
 //     cloudinary: cloudinary,
-//     params: {
-//         folder: 'True',
-//         transformation: [
-//             { width: 555, height: 888, crop: "fill" }
-//         ],
-//         allowedForms: ['jpeg', 'png', 'jpg', 'mp4'],
-//         resource_type: 'auto'
+//     params: async (req, file) => {
+//         const timestamp = Math.floor(Date.now() / 1000); // Current Unix timestamp
+//         const paramsToSign = {
+//             folder: 'uploads', // The folder name
+//             timestamp: timestamp,
+//             transformation: [{ width: 555, height: 888, crop: "fill" }]
+//         };
+//         const signature = generateSignature(paramsToSign);
+
+//         return {
+//             folder: 'uploads',
+//             transformation: [{ width: 555, height: 888, crop: "fill" }],
+//             allowed_formats: ['jpeg', 'png', 'jpg', 'mp4'],
+//             resource_type: 'auto',
+//             api_key: process.env.CLOUDINARY_KEY,
+//             timestamp: timestamp,
+//             signature: signature
+//         };
 //     }
 // });
+
+
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'True',
+        transformation: [
+            { width: 555, height: 888, crop: "fill" }
+        ],
+        allowedForms: ['jpeg', 'png', 'jpg', 'mp4'],
+        resource_type: 'auto',
+        public_id: generateSoketId(16),
+    }
+});
 
 
 module.exports = { cloudinary, storage };
