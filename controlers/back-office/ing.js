4e88b3b0ab54a89cd19bbc3ff18f249352d7b2af
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-21 20:43:55 +0200

    all done

--- controlers/back-office/ing.js
+++ controlers/back-office/ing.js
@@ -123,6 +123,7 @@ module.exports.saveIng = async(req, res, next) => {
         const date = new Date();
         date.setUTCHours(23, 0, 0, 0);
         const ings = await Ingredient.find({locatie: loc})
+        console.log(ings.length)
         console.log(ings[23])
         // const updatePromises = ings.map(ing => {
         //   // const index = ing.inventary && ing.inventary.length ? ing.inventary.length + 1 : 1;
@@ -144,10 +145,10 @@ module.exports.saveIng = async(req, res, next) => {
         // });
 
         for (const ing of ings) {
-          if (!Array.isArray(ing.inventary)) {
+  
            const ingg = await Ingredient.findOneAndUpdate({_id: ing._id} , { $push: { inventary: {qty: ing.qty, date: date, index: 333} } }, {new: true});
            console.log(ingg.inventary.find(i = i.index === 333))
-          }
+          
         }
     
       //  const response = await Promise.all(updatePromises);
