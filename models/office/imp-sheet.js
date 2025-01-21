70ef06783032b160f304d0ac79313d8c8cb9cadb
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-21 18:05:04 +0200

    all

---
+++ models/office/imp-sheet.js
@@ -0,0 +1,95 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Ingredient = require('./inv-ingredient')
+
+
+
+const imparimentSheetSchema = new Schema({
+    date: {
+        type: Date, 
+        index: true
+    },
+    ings: [
+        {   
+            qty: Number,
+            ing: {
+                type: Schema.Types.ObjectId,
+                ref: "IngredientInv"
+            }
+        }
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    locatie: {
+        type: Schema.Types.ObjectId,
+        ref: 'Locatie'
+    },
+    salePoint: {
+        type: Schema.Types.ObjectId,
+        ref: 'SalePoint'
+      }
+});
+
+
+imparimentSheetSchema.pre('deleteOne', { document: false, query: true }, async function (next) {
+        try{
+            const query = this.getQuery()
+            const dbSheet = await this.model.findOne(query)
+                    .populate({path: 'ings.ing', select: 'productIngredient ings name price um tva'})
+                if(dbSheet){
+                const ingsPromises = dbSheet.ings.flatMap(ing => {
+                    if(ing.ing.productIngredient){
+                    return ing.ing.ings.map(ingg =>
+                        Ingredient.findByIdAndUpdate(
+                        ingg.ing,
+                        { $inc: { qty: ingg.qty ? ingg.qty : 0 } },
+                        { new: true }
+                        ).exec()
+                    );
+                    } else {
+                    return Ingredient.findByIdAndUpdate(ing.ing._id, {$inc: {qty: ing.qty ? ing.qty : 0}}, {new: true }).exec()
+                    }
+                })
+                 await Promise.all(ingsPromises)
+                } else{
+                    console.warn('No dbSheet found for query:');
+                }
+            next()
+        } catch(error) {
+            console.error('Error in pre deleteOne hook:', error);
+            next(error)
+        }
+})
+
+
+
+imparimentSheetSchema.post('save', async function (doc, next) {
+    console.log('hit the pre save')
+    try{
+        await doc.populate({path: 'ings.ing', select: 'productIngredient ings name price um tva'})
+        const ingsPromises = doc.ings.flatMap(ing => {
+            if(ing.ing.productIngredient){
+            return ing.ing.ings.map(ingg =>
+                Ingredient.findByIdAndUpdate(
+                ingg.ing,
+                { $inc: { qty: -ingg.qty } },
+                { new: true }
+                ).exec()
+            );
+            } else {
+            return Ingredient.findByIdAndUpdate(ing.ing._id, {$inc: {qty: -ing.qty}}, {new: true }).exec()
+            }
+        })
+        await Promise.all(ingsPromises)
+
+     next()
+    } catch(error) {
+        console.error('Error in post save hook:', error);
+        next(error)
+    }
+})
+
+
+module.exports = mongoose.model("ImpSheet", imparimentSheetSchema);
