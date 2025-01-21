2b0642af42dc9f31fe039b7331d89c8a28daa6d8
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-21 18:14:17 +0200

    ss

--- routes/back-office/ing.js
+++ routes/back-office/ing.js
@@ -9,18 +9,17 @@ router.route('/ingredient')
     .put(ingRoutes.editIng)
     .delete(ingRoutes.deleteIng)
 
-router.route('/print-ing-list').post(printRoutes.createIngredientsInvXcel)
 router.route('/print-consum').post(printRoutes.printConsum)
 
-router.route('/print-ing-list').post(printRoutes.createIngredientsInvXcel)
-router.route('/print-consum').post(printRoutes.printConsum)
+// router.route('/print-ing-list').post(printRoutes.createIngredientsInvXcel)
+// router.route('/print-consum').post(printRoutes.printConsum)
 router.route('/save-inventary').get(ingRoutes.saveInventary)
 router.route('/save-faptic').post(ingRoutes.saveManualInventary)
 router.route('/save-inv').post(ingRoutes.saveInv)
-router.route('/update-ingredient-quantity').post(ingRoutes.updateIngredientQuantity)
+// router.route('/update-ingredient-quantity').post(ingRoutes.updateIngredientQuantity)
 
-router.route('/get-consumabil').get(ingRoutes.getIngConsumabil)
-router.route('/get-inventary').get(ingRoutes.getInventary)
+// router.route('/get-consumabil').get(ingRoutes.getIngConsumabil)
+// router.route('/get-inventary').get(ingRoutes.getInventary)
 
 
 
