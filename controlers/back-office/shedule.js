3d516efa2ce4b04f0b0050a99f8161b33fd9ed40
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-26 16:55:18 +0200

    all done

--- controlers/back-office/shedule.js
+++ controlers/back-office/shedule.js
@@ -70,9 +70,7 @@ module.exports.addPontaj = async (req, res, next) => {
             const date = new Date(year, month, i)
             date.setUTCHours(0,0,0,0)
             const dates = new Date(date)
-            const newDate = new Date(dates.setDate(dates.getDate()+1)).setUTCHours(0,0,0,0);
-            console.log(date)
-            console.log('day', month, i)
+            const newDate = new Date(dates.setDate(dates.getDate())).setUTCHours(0,0,0,0);
             const day = {
                  date: newDate,
                  number: i,
