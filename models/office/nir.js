3efaa5645e6439ede69491d334553abcbafb5628
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-27 11:08:14 +0200

    all done

--- models/office/nir.js
+++ models/office/nir.js
@@ -166,7 +166,7 @@ nirSchema.pre('save', async function (next){
     };
 
     const promises = doc.ingredients.map((el) => {
-      return Ingredient.findByIdAndUpdate(
+      return Ingredient.updateOne(
         { name: el.name, gestiune: el.gestiune },
         {
           $setOnInsert: {
