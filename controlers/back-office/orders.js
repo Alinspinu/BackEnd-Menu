cc965af50ea308e964d2d91692f48f32749350c0
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-27 23:30:32 +0200

    all done

--- controlers/back-office/orders.js
+++ controlers/back-office/orders.js
@@ -99,8 +99,8 @@ module.exports.getOrder = async (req, res, next) => {
 
 module.exports.testRaport = async (req, res) => {
     try {
-        const today = new Date(2025, 0, 1).setUTCHours(0, 0, 0, 0);
-        const end = new Date(2025, 0, 5).setUTCHours(0, 0, 0, 0);
+        const today = new Date(2025, 0, 6).setUTCHours(0, 0, 0, 0);
+        const end = new Date(2025, 0, 12).setUTCHours(0, 0, 0, 0);
         const orders = await Order.find({ 
             locatie: "655e2e7c5a3d53943c6b7c53", 
             updatedAt: { $gte: today, $lte: end }, 
