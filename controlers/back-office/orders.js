8cecf0bd175d5a968d302d3932921aa14454b80e
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-01-27 23:39:12 +0200

    all done

--- controlers/back-office/orders.js
+++ controlers/back-office/orders.js
@@ -59,8 +59,8 @@ module.exports.getOrder = async (req, res, next) => {
 module.exports.testRaport = async (req, res) => {
     try{
         const today = new Date(2025, 0, 1).setUTCHours(0,0,0,0)
-        const end = new Date(2025, 0, 12).setUTCHours(0,0,0,0)
-        const orders = await Order.find({ locatie: "655e2e7c5a3d53943c6b7c53" , updatedAt: {$gte: today, $lte: end}, status: 'done'})
+        const end = new Date(2025, 0, 1).setUTCHours(0,0,0,0)
+        const orders = await Order.find({ locatie: "655e2e7c5a3d53943c6b7c53" , updatedAt: {$gte: today}, status: 'done'})
         for(let order of orders){
             await createProductSaleReport(order.products, order.updatedAt)
          }
