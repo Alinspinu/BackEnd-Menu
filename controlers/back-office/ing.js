6af4639bdd6021a4b9d3ac846185fd11918917a2
unknown <alinz.spinu@gmail.com>
2025-01-30 21:35:12 +0200

    all done

--- controlers/back-office/ing.js
+++ controlers/back-office/ing.js
@@ -130,7 +130,7 @@ module.exports.saveIng = async(req, res, next) => {
         const date = new Date();
         date.setUTCHours(23, 0, 0, 0, 0);
         const formattedDate = date.toISOString();
-        const ings = await Ingredient.find({locatie: loc, productIngredient: false}).select('inventary name gestiune dep um')
+        const ings = await Ingredient.find({locatie: loc, productIngredient: false}).select('inventary name gestiune qty dep um')
         const updatePromises = ings.map(ing => {
           let index = 1;
           if (ing.inventary && ing.inventary.length) {
