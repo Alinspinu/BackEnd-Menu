b5a6c8230c6ed486ef2111ad01ac911e66dda69a
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-02-08 21:56:56 +0200

    all done

--- controlers/users/users.js
+++ controlers/users/users.js
@@ -100,7 +100,7 @@ const Order = require('../../models/office/product/order')
 
 module.exports.fixBul = async (req, res, next) => {
     try{
-
+        console.log(req)
         const user = await User.findOne({email: "alin@flowmanager.ro"})
         const startDate = new Date(2024, 9, 1)
         const orders = await Order.find({createdAt: {$gte: startDate}})
