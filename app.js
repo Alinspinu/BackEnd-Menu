55f1af57b82e940c71327bcbe39282a5acd0b01c
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-02-08 21:38:47 +0200

    all done

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.use('/auth', authRoutes);
 app.use('/nutrition', nutritionRoutes);
 app.use('/register', authApi, registerRoutes);
 app.use('/table', authApi, tableRoutes);
-app.use('/users', authApi, usersRoutes);
+app.use('/users',  usersRoutes);
 app.use('/notification', authApi, notifRoutes)
 app.use("/top", toppingRoutes);
 app.use('/suplier', authApi, suplierRoutes);
