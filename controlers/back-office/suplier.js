c241a671a147a0a488f9a89f661b4600e7f8355e
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-02-25 21:54:01 +0200

    all done

--- controlers/back-office/suplier.js
+++ controlers/back-office/suplier.js
@@ -108,7 +108,7 @@ module.exports.addSuplier = async (req, res, next) => {
     const {id, records} = req.body
     try{
         for(let record of records) {
-            if(record.document.amount === 3445.28){
+            if(record.document.amount === 1124.38){
                 console.log(record)
             }
         }
