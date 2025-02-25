e8d498365aafd2f7ce1abf68bfc1e26d6cb74b03
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-02-25 22:01:29 +0200

    all done

--- controlers/back-office/suplier.js
+++ controlers/back-office/suplier.js
@@ -107,12 +107,13 @@ module.exports.addSuplier = async (req, res, next) => {
    module.exports.updateSuplierRecords = async (req, res) => {
     const {id, records} = req.body
     try{
-        for(let record of records) {
-            if(record.document.amount === 1124.38){
+        const parsedRecords = JSON.parse(records)
+        for(let record of parsedRecords){
+            if(record.document.amount === 1124.38 ){
                 console.log(record)
             }
         }
-       const suplier = await Suplier.findByIdAndUpdate(id, {$set: {records: records}}, {new: true})
+       const suplier = await Suplier.findByIdAndUpdate(id, {$set: {records: parsedRecords}}, {new: true})
         if(!suplier){
             res.status(404).json({message: 'Furnizorul nu a fost găsit!'})
         } else {
