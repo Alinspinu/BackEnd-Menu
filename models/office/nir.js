0ad3fb945dfe6396ad11f1859f7d06f795e03d44
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-02-26 16:56:40 +0200

    all done

--- models/office/nir.js
+++ models/office/nir.js
@@ -143,14 +143,38 @@ nirSchema.pre('save', async function (next){
       nir: doc._id 
     }
 
-    const suplier = await Suplier.findByIdAndUpdate(
-      doc.suplier,  
-      { 
-          $push: { records: record },
-          $inc: { sold: doc.totalDoc } 
-      },
-      { new: true, useFindAndModify: false }
-    )
+    // const suplier = await Suplier.findByIdAndUpdate(
+    //   doc.suplier,  
+    //   { 
+    //       $push: { records: record },
+    //       $inc: { sold: doc.totalDoc } 
+    //   },
+    //   { new: true, useFindAndModify: false }
+    // )
+
+
+      if (sup) {
+        sup.records.push(record)
+        const sortedRecords = sup.records.sort((a, b) => {
+          const aDate = new Date(a.date).getTime() 
+          const bDate = new Date(b.date).getTime()
+          return aDate - bDate
+      })
+          const recordIndex = sortedRecords.findIndex(r => r.nir.toString() === doc._id.toString());
+          console.log('Rcord index nir save schema', recordIndex)
+          if (recordIndex !== -1) {
+              for (let i = recordIndex; i < sortedRecords.length; i++) {
+                  console.log('recodrd after', sortedRecords[i].sold)
+                  sortedRecords[i].sold += doc.totalDoc;
+                  console.log('recodrd before', sortedRecords[i].sold)
+              }
+              sup.sold = sup.sold + doc.totalDoc
+              sup.records = sortedRecords
+              await sup.save();
+          }
+      }
+
+
 
     console.log("Supplier update:", suplier.name);
 
@@ -239,7 +263,7 @@ nirSchema.pre('deleteOne', { document: true, query: false }, async function(next
           return aDate - bDate
       })
           const recordIndex = sortedRecords.findIndex(r => r.nir.toString() === doc._id.toString());
-          console.log('Rcord index nir schema', recordIndex)
+          console.log('Rcord index  nir delete schema', recordIndex)
           if (recordIndex !== -1) {
               sortedRecords.splice(recordIndex, 1);
               for (let i = recordIndex; i < sortedRecords.length; i++) {
