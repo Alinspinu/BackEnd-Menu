0eaaeb979e97f35c77db4ee1296cf737848c98e6
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-03-01 20:41:54 +0200

    all done

--- utils/reports.js
+++ utils/reports.js
@@ -637,7 +637,6 @@ async function createDayReport(billProducts, ingredients, loc, bills, dat, point
                 taxValue: round(baseTax / daysNumber),
                 user: dbUser._id,
             }
-            console.log(dbEmpl)
             const existingUser = users.find(u => u.name === dbEmpl.name)
             if(existingUser){
                 existingUser.totalIncome = round(existingUser.totalIncome + dbEmpl.totalIncome)
