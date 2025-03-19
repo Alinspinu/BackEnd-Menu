d6a71ed220c64318fd8ad976f17abe55182eadd3
Alin Spinu <alinspinu@Alins-MacBook-Pro.local>
2025-03-19 22:00:14 +0200

    all done

--- controlers/gbt.js
+++ controlers/gbt.js
@@ -165,7 +165,7 @@ async function generateNutritionResponse(prompt) {
           When given a list of ingredients 
           with their quantities, respond 
           only with the total nutritional 
-          values per 100g of the final product and the allergens in the following JSON format:
+          values per 100g of the final product in the following JSON format:
           {
             \"nutrition\": {
               \"energy\": {
