2bdf979ce24146e5b46c107714cfef9673f3017f
Alin Spinu <alinz.spinu@gmail.com>
2025-05-02 19:16:05 +0300

    all done

--- controlers/back-office/cash-register.js
+++ controlers/back-office/cash-register.js
@@ -121,7 +121,7 @@ module.exports.addEntry = async (req, res, next) => {
         entryDate.setUTCHours(0,0,0,0)
         const nextDay = new Date(entryDate);
         nextDay.setDate(entryDate.getDate() + 1);
-        const day = await Day.findOne({ date: { $gte: entryDate, $lt: nextDay}, locatie: locatie }, null, {new: true}).populate({ path: 'entry' })
+        const day = await Day.findOne({ date: { $gte: entryDate, $lt: nextDay}, locatie: locatie, salePoint: salePoint }, null, {new: true}).populate({ path: 'entry' })
         if (day) {
             const daySum = day.entry.reduce((total, doc) => total + doc.amount, 0)
             day.entry.push(newEntry)
