f65e2e6439fd48540007428c40f19695ffa5c777
Alin Spinu <alinz.spinu@gmail.com>
2025-05-02 11:52:35 +0300

    all done

--- controlers/back-office/cats.js
+++ controlers/back-office/cats.js
@@ -6,7 +6,7 @@ const cloudinary = require('cloudinary').v2;
 module.exports.sendCats = async (req, res, next) => {
     try {
         const { loc, point } = req.query;
-        const cats = await Cat.find({locatie: loc}).populate({
+        const cats = await Cat.find({locatie: loc, salePoint: point}).populate({
             path: 'product',
             populate: [
                 { path: 'category' },
