e8810b711a6a71223a8d22c329924af139b3f7e1
Alin Spinu <alinz.spinu@gmail.com>
2025-05-02 09:46:19 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -88,7 +88,7 @@ module.exports.printNir = async (req, res, next) => {
       width: 280,
     });
 
-    doc.fontSize(10).text(`PUNCT DE LUCRU: ${nir.salePoint.name}`, 700, 15, {
+    doc.fontSize(9).text(`Punct de lucru: ${nir.salePoint.name.toUpperCase()}`, 600, 15, {
       width: 280,
     });
   
