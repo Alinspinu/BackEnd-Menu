bc7b8973ed60931dd7f15a245f8c00668c655dde
Alin Spinu <alinz.spinu@gmail.com>
2025-05-11 17:12:50 +0300

    sll

--- models/office/nir.js
+++ models/office/nir.js
@@ -167,7 +167,7 @@ nirSchema.pre('save', async function (next){
 
 
     const counter = await Counter.findOneAndUpdate(
-      { locatie: doc.locatie, model: "Nir", salePonit: doc.salePoint },
+      { locatie: doc.locatie, model: "Nir", salePonint: doc.salePoint },
       { $inc: { value: 1 } },
       { upsert: true, new: true }
     );
