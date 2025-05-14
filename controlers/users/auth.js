24764a5e97f9df11a24faeb2c3ec88434b39af3e
Alin Spinu <alinz.spinu@gmail.com>
2025-05-14 11:02:32 +0300

    all done

--- controlers/users/auth.js
+++ controlers/users/auth.js
@@ -130,9 +130,9 @@ module.exports.verifyOTP = async (req, res) => {
             const now = new Date().getTime()
             const otpDate = new Date(user.otp.date).getTime()
             if( now - otpDate < 300000){
-                res.status(200).json({message: 'valid'})
+                res.status(200).json({message: 'valid', id: user._id})
             } else {
-                res.status(200).json({message: 'expired'})
+                res.status(200).json({message: 'expired', id: user._id})
             }
         } else {
             res.status(200).json({message: 'invalid'})
