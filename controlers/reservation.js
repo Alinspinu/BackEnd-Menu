ebe507da34617ce353a0df3e31ae666c7ebf4c62
Alin Spinu <alinz.spinu@gmail.com>
2025-05-24 16:09:54 +0300

    all done

--- controlers/reservation.js
+++ controlers/reservation.js
@@ -30,7 +30,7 @@ module.exports.addReservationFromClient = async(req, res)  => {
         const newReservation = new Reservation(reservation)
         const startTime = new Date(reservation.date).getTime() - 2 * 60 * 60 * 1000
         const endTime = new Date(reservation.date).getTime() + 2 * 60 * 60 * 1000
-        const reservations = await Reservation.find({salePoint: reservation.salePoint, date: {$gte: startTime, $lte: endTime}})
+        const reservations = await Reservation.find({salePoint: reservation.salePoint, status: 'accepted', date: {$gte: startTime, $lte: endTime}})
         console.log(reservation.length)
         let pendding = ' '
         if(reservations.length > 4){
