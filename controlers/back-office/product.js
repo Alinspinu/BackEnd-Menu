816cd3a5528de2119b776cb1057d83a0c1fe8a55
Alin Spinu <alinz.spinu@gmail.com>
2025-05-28 11:40:27 +0300

    all done

--- controlers/back-office/product.js
+++ controlers/back-office/product.js
@@ -18,9 +18,9 @@ module.exports.updateProducts = async (req, res) => {
         console.log('produse gasite', products.length)
         for(const product of products){
             for( const sub of product.subProducts){
-                if(sub.name === 'True Blend'){
-                //    const description = 'Țară de origine: Columbia, | Procesare: Fermentare Naturală, | Decofeinizată cu: Acetat de etil provenit din trestie de zahăr, | Profil aromatic: [caramel sărat, alune, scorțișoară și coji de portocală.]'
-                const description = 'Blendul emblematic [True Fine Coffee], format din 80% Brazilia Agua Limpa și 20% Papua Noua Guinee. Are un corp [intens] cu note de [alune de pădure, cacao și ciocolată], perfect completate de crema de lapte, care îl transformă într-o experiență de neuitat.'
+                if(sub.name === 'Decofeinizat'){
+                   const description = 'Țară de origine: Columbia, | Procesare: Fermentare Naturală, | Decofeinizată cu: Acetat de etil provenit din trestie de zahăr, | Profil aromatic: [caramel sărat, alune, scorțișoară și coji de portocală.]'
+                // const description = 'Blendul emblematic [True Fine Coffee], format din 80% Brazilia Agua Limpa și 20% Papua Noua Guinee. Are un corp [intens] cu note de [alune de pădure, cacao și ciocolată], perfect completate de crema de lapte, care îl transformă într-o experiență de neuitat.'
                     await SubProduct.findByIdAndUpdate(sub._id, {description: description})
                 }
             }
