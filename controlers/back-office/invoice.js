0077972d147bbd5444c57d1345f9a293819e1e79
Alin Spinu <alinz.spinu@gmail.com>
2025-06-10 19:57:49 +0300

    all done

--- controlers/back-office/invoice.js
+++ controlers/back-office/invoice.js
@@ -376,7 +376,7 @@ function createInvoice(order, customer, supplier) {
         unitCode: 'H87',
         price: priceNoVat,
         vatPrecent: p.tva,
-        total: +p.total - p.discount, 
+        total: +p.total, 
         totalNoVat: roundd(priceNoVat * p.quantity)
       };
       if(p.discount > 0){
