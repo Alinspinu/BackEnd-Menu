270b5469b380d729fa1bdeaa04f58b12c006faa7
Alin Spinu <alinz.spinu@gmail.com>
2025-06-11 21:40:33 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -1319,7 +1319,7 @@ module.exports.factura = async (req, res, next) => {
   doc.rect(25, 669, 338, 105)
   doc.lineWidth(0.5);
   doc.stroke()
-  doc.image('public/icons/logo-stanga.png', 35, 700, {width: 300})
+  doc.image('public/icons/logo-stanga.png', 35, 689, {width: 300})
 
   doc.rect(363, 669, 197, 105)
   doc.lineWidth(0.5);
