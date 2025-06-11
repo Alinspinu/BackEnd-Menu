7f6cfe1615d659f59306fb12488ca30071b0be1a
Alin Spinu <alinz.spinu@gmail.com>
2025-06-11 20:33:33 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -1116,20 +1116,21 @@ module.exports.factura = async (req, res, next) => {
   doc.fontSize(10);
   // doc.font('Courier-Bold')
   doc.text('Client:', 395 - 40, 50, { width: 35, align: "left" })
-  doc.text(`C.I.F.:`, 395 - 40, 82 - 7, { width: 30, align: "left" });
-  doc.text(`Nr. Reg. Com.:`, 395 - 40, 94 - 7, { width: 90, align: "left" });
-  doc.text(`Adresa:`, 395 - 40, 106 - 7, { width: 40, align: "left" })
+  doc.text(`C.I.F.:`, 395 - 40, 62 - 7, { width: 30, align: "left" });
+  doc.text(`Nr. Reg. Com.:`, 395 - 40, 74 - 7, { width: 90, align: "left" });
+  doc.text(`Adresa:`, 395 - 40, 86 - 7, { width: 40, align: "left" })
 
 
 
   //date client
   doc.fontSize(10);
-  doc.font("public/font/Montserrat-Regular.ttf");
+  // doc.font('public/font/Montserrat-Bold.ttf')
   doc.text(`${invoice.client.name}`, 485 - 40, 50, { width: 215, align: "left" });
-  doc.text(`${invoice.client.vatNumber}`, 485 + 10 - 40, 82 - 7, { width: 145, align: "left" });
-  doc.text(`${invoice.client.registration}`, 485 + 10 - 40, 94 - 7, { width: 105, align: "left" });
+  doc.font("public/font/Montserrat-Regular.ttf");
+  doc.text(`${invoice.client.vatNumber}`, 485  - 40, 62 - 7, { width: 145, align: "left" });
+  doc.text(`${invoice.client.registration}`, 485  - 40, 74 - 7, { width: 105, align: "left" });
   // doc.font("Courier");
-  doc.text(`${invoice.client.address.street || ''}`, 395 - 40, 106 + 5, { width: 215, align: "left" })
+  doc.text(`${invoice.client.address.street || ''}`, 395 - 40, 86 + 5, { width: 215, align: "left" })
 
 
 
