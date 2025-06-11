d9c88eaf89b3aee65cfc076311b4e86a49589efa
Alin Spinu <alinz.spinu@gmail.com>
2025-06-11 21:31:53 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -1147,14 +1147,14 @@ module.exports.factura = async (req, res, next) => {
   doc.font("public/font/Montserrat-Regular.ttf");
   doc.text('Serie/Nr.', 230, 192 + 35, { width: 40, align: "left" })
   doc.text('Emisă la', 230, 207 + 35, { width: 40, align: "left" })
-  doc.text('Scadentă la', 230, 222 + 35, { width: 40, align: "left" })
+  doc.text('Scadentă la', 230, 222 + 35, { width: 50, align: "left" })
 
   // Titlu Factura Date
   doc.font('public/font/Montserrat-Bold.ttf')
   doc.fontSize(10)
   doc.text(`${invoice.invoiceNumber}`, 275, 190 + 35)
-  doc.text(`${invoice.issueDate}`, 275, 205 + 35)
-  doc.text(`${invoice.dueDate}`, 275, 220 + 35)
+  doc.text(`${invoice.issueDate}`, 285, 205 + 35)
+  doc.text(`${invoice.dueDate}`, 285, 220 + 35)
 
   //header produsex
   doc.rect(25, 280, 18, 30)
