8537740fc5d659d75e4e3e04cf1fe1ae5a7014ac
Alin Spinu <alinz.spinu@gmail.com>
2025-06-11 11:43:37 +0300

    all done

--- models/office/invoice.js
+++ models/office/invoice.js
@@ -154,5 +154,21 @@ invoiceSchema.pre("save", async function (next) {
     }
 });
 
+invoiceSchema.pre('findOneAndDelete', async function(next) {
+    try{
+
+        const doc = this;
+        const counter = await Counter.findOneAndUpdate( 
+            { locatie: this.locatie, model: "Invoice", salePoint: this.salePoint },
+            { $inc: { value: -1 } },
+            { upsert: true, new: true }
+        ).exec();
+        console.log(counter)
+        next();
+    } catch(error) {
+        next(error)
+    }
+  });
+
 
 module.exports = mongoose.model("Invoice", invoiceSchema);
