5bb87ff1a990055c308fdb7d2b8e3df45b493945
Alin Spinu <alinz.spinu@gmail.com>
2025-06-12 17:24:04 +0300

    all done

--- controlers/back-office/invoice.js
+++ controlers/back-office/invoice.js
@@ -54,6 +54,7 @@ module.exports.uploadInvoiceToEFactura = async (req, res) => {
   try{
     const invoice = await Invoice.findById(id)
     const xml = buildEFacturaHeaderXML(invoice)
+    console.log(xml)
     const response = await testInvoice(xml)
     invoice.eFacturaId = response.eFacturaId
     invoice.eFacturaError = response.eFacturaError
