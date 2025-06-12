32388d387f381dbec3526878dacfe2356f211396
Alin Spinu <alinz.spinu@gmail.com>
2025-06-12 21:27:04 +0300

    all done

--- controlers/back-office/client.js
+++ controlers/back-office/client.js
@@ -25,3 +25,14 @@ module.exports.getClients = async (req, res) => {
         res.status(200).json(error)
     }
 }
+
+module.exports.editClient = async (rea, res) => {
+    const {client} = req.body
+    try{
+        const savedClient = await Client.findByIdAndUpdate(client._id, client, {new: true})
+        res.status(200).json({message:'Clientu a fost editat cu success!', client: savedClient })
+    } catch(error){
+        console.log(error)
+        res.status(200).json(error)
+    }
+}
--- routes/back-office/client.js
+++ routes/back-office/client.js
@@ -7,6 +7,7 @@ const clients = require('../../controlers/back-office/client')
 router.route('/')
     .post(clients.addClient)
     .get(clients.getClients)
+    .put(clients.editClient)
 
 
 module.exports = router
