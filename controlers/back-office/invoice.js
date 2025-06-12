72c95d5f33e23c924ca114b10511d6aadf3faeb5
Alin Spinu <alinz.spinu@gmail.com>
2025-06-12 18:00:07 +0300

    all done

--- controlers/back-office/invoice.js
+++ controlers/back-office/invoice.js
@@ -357,7 +357,7 @@ module.exports.getInvoice = async (req, res) => {
 
 async function downloadZipFileCheck(id) {
   try {
-    const response = await axios.get(`https://api.anaf.ro/test/FCTEL/rest/descarcare?id=${'3037598428'}`, {
+    const response = await axios.get(`https://api.anaf.ro/test/FCTEL/rest/descarcare?id=${'3037623422'}`, {
       responseType: 'arraybuffer',
       headers: {
         'Authorization': `Bearer ${process.env.TOKEN_ANAF}`,
