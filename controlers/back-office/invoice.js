4f7f2ee4e58e9fdb4721d52a0c955ad1fe58e0ca
Alin Spinu <alinz.spinu@gmail.com>
2025-06-13 16:52:36 +0300

    all d

--- controlers/back-office/invoice.js
+++ controlers/back-office/invoice.js
@@ -666,11 +666,9 @@ function buildEFacturaHeaderXML(invoice) {
   // LegalMonetaryTotal
   const total = doc.ele('cac:LegalMonetaryTotal');
   total.ele('cbc:LineExtensionAmount', { currencyID: invoice.currencyId }).txt(invoice.taxExclusiveAmount).up();
-  total.ele('cbc:TaxExclusiveAmount', { currencyID: invoice.currencyId }).txt(invoice.taxExclusiveAmount).up();
-
-  if(totalDiscount > 0) total.ele('cbc:AllowanceTotalAmount', { currencyID: invoice.currencyId }).txt(totalDiscount).up();
-  
+  total.ele('cbc:TaxExclusiveAmount', { currencyID: invoice.currencyId }).txt(invoice.taxExclusiveAmount).up();  
   total.ele('cbc:TaxInclusiveAmount', { currencyID: invoice.currencyId }).txt(invoice.taxInclusiveAmount).up();
+  if(totalDiscount > 0) total.ele('cbc:AllowanceTotalAmount', { currencyID: invoice.currencyId }).txt(totalDiscount).up();
   total.ele('cbc:PrepaidAmount', { currencyID: invoice.currencyId }).txt(0).up();
   total.ele('cbc:PayableAmount', { currencyID: invoice.currencyId }).txt(invoice.taxInclusiveAmount).up();
 
