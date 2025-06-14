e98053e663d94ef29f1bb530eb3d1d929b1c02b6
Alin Spinu <alinz.spinu@gmail.com>
2025-06-14 21:12:42 +0300

    all d

--- controlers/back-office/invoice/upload.js
+++ controlers/back-office/invoice/upload.js
@@ -63,7 +63,7 @@ async function uploadInvoice(xml, cn = false) {
           eFacturaStatus: eFacturaStatus,
           eFacturaId: downloadId ? downloadId : indexIncarcare,
           eFacturaError: '',
-          message: upload ? 'Documentul a fost încărcat cu success!' : `Documentul a fost verificat, STATUS: ${eFacturaStatus}`
+          message: upload ? 'Documentul a fost încărcat cu success!' : `Documentul a fost verificat, STATUS: `
         }
     } catch(error) {
       console.error('Error uploading:', err?.response?.data || err.message);
