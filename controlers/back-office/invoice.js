bc64755309259f6913a8861af0fec371950ed5c9
Alin Spinu <alinz.spinu@gmail.com>
2025-06-14 10:13:07 +0300

    all d

--- controlers/back-office/invoice.js
+++ controlers/back-office/invoice.js
@@ -745,21 +745,21 @@ function buildEFacturaHeaderXML(invoice) {
 
 
 
+
+
+
 function buildEFacturaCreditNoteXML(invoice, creditNote) {
   const doc = create({ version: '1.0' })
-    .ele('Invoice', {
-      xmlns: 'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2',
-      'xmlns:cac': 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2',
-      'xmlns:cbc': 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2',
-      'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-      'xsi:schemaLocation': 'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2 UBL-Invoice-2.1.xsd'
-    })
-
+      .ele('Invoice', {
+        xmlns: 'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2',
+        'xmlns:cac': 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2',
+        'xmlns:cbc': 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2'
+      });
       doc.ele('cbc:CustomizationID').txt('urn:cen.eu:en16931:2017#compliant#urn:efactura.mfinante.ro:CIUS-RO:1.0.1');
       doc.ele('cbc:ProfileID').txt('urn:fdc:peppol.eu:2017:poacc:billing:01:1.0');
-      doc.ele('cbc:ID').txt(creditNote.id);
+      doc.ele('cbc:ID').txt(invoice.invoiceNumber + "S");
       doc.ele('cbc:IssueDate').txt(creditNote.date);
-      doc.ele('cbc:InvoiceTypeCode').txt('381'); // credit note
+      doc.ele('cbc:InvoiceTypeCode').txt('380'); // credit note
       doc.ele('cbc:DocumentCurrencyCode').txt('RON');
       doc.ele('cbc:TaxCurrencyCode').txt('RON');
 
