1723216cf9ab0ecdc073e16363f5f2ca0c3bb1cb
Alin Spinu <alinz.spinu@gmail.com>
2025-06-16 00:15:34 +0300

    alll done

--- controlers/back-office/client.js
+++ controlers/back-office/client.js
@@ -41,7 +41,9 @@ module.exports.editClient = async (req, res) => {
 module.exports.getClient = async (req, res) => {
     const {id} = req.query
     try{
+        console.log(id)
         const client = await Client.findById(id)
+        console.log(client)
         res.status(200).json(client)
     } catch(error){
         console.log(error)
