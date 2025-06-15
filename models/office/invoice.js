095d6a8089398680a0d0d3d9e2726a09a64b4b7f
Alin Spinu <alinz.spinu@gmail.com>
2025-06-15 12:36:50 +0300

    alll done

--- models/office/invoice.js
+++ models/office/invoice.js
@@ -256,7 +256,7 @@ invoiceSchema.pre('findOneAndDelete', async function(next){
           const bDate = new Date(b.date).getTime()
           return aDate - bDate
       })
-          const recordIndex = sortedRecords.findIndex(r => r.customer.toString() === doc._id.toString());
+          const recordIndex = sortedRecords.findIndex(r => r.invoice.toString() === doc._id.toString());
           if (recordIndex !== -1) {
               sortedRecords.splice(recordIndex, 1);
               for (let i = recordIndex; i < sortedRecords.length; i++) {
