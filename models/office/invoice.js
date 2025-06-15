0eb4a9fdce9fad7b5dd59f0d6313754c748f04e3
Alin Spinu <alinz.spinu@gmail.com>
2025-06-15 12:07:40 +0300

    alll done

--- models/office/invoice.js
+++ models/office/invoice.js
@@ -3,12 +3,19 @@ const Schema = mongoose.Schema;
 
 const Counter = require('../utils/counter')
 
+const Client = require('../office/client')
+
+
+const {unloadIngs, createProductSaleReport, uploadIngs} = require('../../utils/inventary');
+const product = require("./product/product");
+
 const invoiceSchema = new Schema({
     invoice: {
         type: Boolean,
         default: true,
         required: true
     },
+    unload: Boolean,
     creditNoteRef: String,
     invoiceNumber: String,
     serie: String,
@@ -91,6 +98,17 @@ const invoiceSchema = new Schema({
             totalNoVat: Number,
             vatPrecent: Number,
             total: Number,
+            productId: String,
+            subProductId: String,
+            ings: [
+                {
+                    qty: Number,
+                    ing: {
+                        type: Schema.Types.ObjectId,
+                        ref: 'IngredientInv'
+                    }
+                }
+            ],
             discount: {
                 value: Number,
                 reason: String,
@@ -142,60 +160,131 @@ const invoiceSchema = new Schema({
 }, {timestamps: true})
 
 
-invoiceSchema.pre("save", async function (next) {
-    try {
-        const doc = this;
-            const counter = await Counter.findOneAndUpdate( 
-                { locatie: this.locatie, model: "Invoice", salePoint: this.salePoint },
-                { $inc: { value: 1 } },
-                { upsert: true, new: true }
-            ).exec();
-            if(counter){
-                doc.index = counter.value;
-                if(!doc.invoiceNumber){
-                    doc.invoiceNumber = `${doc.serie} nr. ${doc.index}`
-                    doc.paymentMeans.serie = `${doc.serie} nr. ${doc.index}`
-                } else {
-                    doc.paymentMeans.serie = doc.invoiceNumber
-                }
-            }else {
-                const newCounter = new Counter({
-                    locatie: doc.locatie,
-                    salePoint: doc.salePoint,
-                    model: 'Invoice',
-                    value: 1
-                })
-                await newCounter.save()
-                doc.index = 1
-                if(!doc.invoiceNumber){
-                    doc.invoiceNumber = `${doc.serie} nr. ${doc.index}`
-                    doc.paymentMeans.serie = `${doc.serie} nr. ${doc.index}`
-                } else {
-                    doc.paymentMeans.serie = doc.invoiceNumber
-                }
-            }
-            next();
-    } catch (error) {
-        next(error);
-    }
-});
 
-invoiceSchema.pre('findOneAndDelete', async function(next) {
-    try{
-        const doc = await this.model.findOne(this.getQuery());
-        if(doc) {
-            const counter = await Counter.findOneAndUpdate( 
-                { locatie: doc.locatie, model: "Invoice", salePoint: doc.salePoint },
-                { $inc: { value: -1 } },
-                { upsert: true, new: true }
-            ).exec();
-            console.log(counter)
-        }
-        next();
-    } catch(error) {
-        next(error)
+
+invoiceSchema.pre('save', async function (next){
+  try{
+    const doc = this
+    const client = await Client.findById(doc.customer)
+
+    const record = {
+      typeOf: 'intrare',
+      document: {
+        typeOf: 'Factura',
+        docId: doc.invoiceNumber,
+        amount: doc.taxInclusiveAmount,
+      },
+      sold: client.sold,
+      date: doc.issueDate,
+      invoice: doc._id, 
+      salePoint: doc.salePoint
     }
-  });
+      if (client) {
+        client.records.push(record)
+        const sortedRecords = sup.records.sort((a, b) => {
+          const aDate = new Date(a.date).getTime() 
+          const bDate = new Date(b.date).getTime()
+          return aDate - bDate
+      })
+          const recordIndex = sortedRecords.findIndex(r => r.invoice.toString() === doc._id.toString());
+          if (recordIndex !== -1) {
+              for (let i = recordIndex; i < sortedRecords.length; i++) {
+                  sortedRecords[i].sold += doc.taxInclusiveAmount;
+              }
+              client.sold = client.sold + doc.taxInclusiveAmount
+              client.records = sortedRecords
+              await client.save();
+              console.log("Client update:", client.name);
+          } else {
+            console.error('ERROR record not found, Client unchanged!')
+          }
+      }
+
+    const counter = await Counter.findOneAndUpdate(
+      { locatie: doc.locatie, model: "Invoice", salePoint: doc.salePoint },
+      { $inc: { value: 1 } },
+      { upsert: true, new: true }
+    );
+    doc.index = counter.value;
+
+    for(let p of doc.products){
+        await unloadIngs(p.ings, p.quantity)
+    }
+
+    // const billProducts = doc.products.map(p => {
+    //     const product = {
+    //         name: p.name,
+    //         productId: p.productId,
+    //         subProductId: p.subProductId,
+    //         discount: 0,
+    //         price: p.price,
+    //         quantity: p.quantity
+    //     }
+    //     return product
+    // })
+
+    // await createProductSaleReport(billProducts, new Date())
+
+
+    next()
+  } catch(error){
+    next(error)
+  }
+})
+
+
+
+
+
+invoiceSchema.pre('findOneAndDelete', async function(next){
+  try{
+    const doc = await this.model.findOne(this.getQuery());
+    console.log('doc in pre delete', doc)
+
+    const client = await Client.findById(doc.customer);
+
+      if (client) {
+        const sortedRecords = client.records.sort((a, b) => {
+          const aDate = new Date(a.date).getTime() 
+          const bDate = new Date(b.date).getTime()
+          return aDate - bDate
+      })
+          const recordIndex = sortedRecords.findIndex(r => r.customer.toString() === doc._id.toString());
+          if (recordIndex !== -1) {
+              sortedRecords.splice(recordIndex, 1);
+              for (let i = recordIndex; i < sortedRecords.length; i++) {
+                  sortedRecords[i].sold -= doc.taxInclusiveAmount;
+              }
+              client.sold = client.sold - doc.taxInclusiveAmount
+              client.records = sortedRecords
+              await client.save();
+              console.log('furnizorul a fos actualizat', client.name)
+          } else {
+            console.error('ERROR! Record not found! Suplier unchanged!')
+          }
+      }
+
+
+      for(let p of doc.products){
+        await uploadIngs(p.ings, p.quantity)
+    }
+
+      const counter = await Counter.findOneAndUpdate( 
+        { locatie: doc.locatie, model: "Invoice", salePoint: doc.salePoint },
+        { $inc: { value: -1 } },
+        { upsert: true, new: true }
+    ).exec();
+
+    doc.index = counter.value;
+
+
+    next()
+  } catch(error){
+    next(error)
+  }
+})
+
+
 
 
 module.exports = mongoose.model("Invoice", invoiceSchema);
