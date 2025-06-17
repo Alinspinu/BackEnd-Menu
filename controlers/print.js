f0072f5218697f70233c87484d1678f80d038ab5
Alin Spinu <alinz.spinu@gmail.com>
2025-06-17 21:01:59 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -1434,12 +1434,12 @@ function createRecipt(recipt){
   doc.text('CHITANȚĂ', 350, height + 35)
   doc.fontSize(10)
   doc.font('public/font/Montserrat-Regular.ttf')
-  doc.text('Serie', 335, height + 60)
-  doc.text('Număr', 400, height + 60)
+  doc.text('Serie', 330, height + 60)
+  doc.text('Număr', 395, height + 60)
   doc.fontSize(11)
   doc.font('public/font/Montserrat-Bold.ttf')
-  doc.text(`${recipt.serie}`, 365, height + 60)
-  doc.text(`${recipt.number.toString().padStart(4, '0')}`, 440, height + 60)
+  doc.text(`${recipt.serie}`, 360, height + 60)
+  doc.text(`${recipt.number.toString().padStart(4, '0')}`, 435, height + 60)
   
 
 
