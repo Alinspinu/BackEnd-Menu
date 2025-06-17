85cfca869d06fa6c752be6591fa485eca3f62a08
Alin Spinu <alinz.spinu@gmail.com>
2025-06-17 20:15:22 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -1413,8 +1413,8 @@ function createRecipt(recipt){
 
   doc.dash(5, { space: 3 });
 
-  doc.moveTo(35, 305)
-     .lineTo(525, 305)
+  doc.moveTo(0, 305)
+     .lineTo(725, 305)
      .stroke();
   doc.undash();
 
