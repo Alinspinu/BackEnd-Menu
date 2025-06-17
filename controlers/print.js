35bd276e913af6b1ff9a9c7e0c49492f7e237304
Alin Spinu <alinz.spinu@gmail.com>
2025-06-17 22:33:37 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -1470,7 +1470,7 @@ function createRecipt(recipt){
     doc.text('Adică', 40, height + 58 + 24)
     doc.text(`Reprezentând`, 40, height + 72 +24)
     doc.fontSize(9)
-    doc.text(`${description}, 0023 din 2025-05-12, 0023 din 2025-05-12, 0023 din 2025-05-12`, 115, height + 74 +24, {width: 220, align: 'left'})
+    doc.text(`${description}, 0023 din 2025-05-12, 0023 din 2025-05-12, 0023 din 2025-05-12`, 115, height + 72 +24, {width: 250, align: 'left'})
 
     doc.fontSize(7)
     doc.text(`${recipt.client.customer.address}`, 80, height + 22 +24, {width: 220, align: 'left'})
