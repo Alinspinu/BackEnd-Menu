71edd1c2a2199c416eadb40c444861c35dbf5502
Alin Spinu <alinz.spinu@gmail.com>
2025-06-17 20:02:41 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -1412,7 +1412,7 @@ function createRecipt(recipt){
   doc.fontSize(18);
   doc.font('public/font/Montserrat-Bold.ttf')
   doc.text(`${recipt.locatie.bussinessName}`, 25 + 10, 25);
-  doc.rect(35, 45, 600, 250)
+  doc.rect(35, 45, 400, 250)
   doc.lineWidth(1.3);
   doc.stroke()
   // doc.moveTo(25 + 10, 45).lineTo(560, 45).stroke();
