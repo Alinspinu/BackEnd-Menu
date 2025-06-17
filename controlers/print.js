5629e475406bf2e54dec677f0f78c24c811f995a
Alin Spinu <alinz.spinu@gmail.com>
2025-06-17 22:43:07 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -1484,7 +1484,7 @@ function createRecipt(recipt){
 
     doc.lineWidth(0.8);
     doc.moveTo(400, height)
-       .lineTo(400, height + 245)
+       .lineTo(400, height + 85)
        .stroke();
 
 
