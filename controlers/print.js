75d9e5236aa593e7ec459a42695ad74121f69518
Alin Spinu <alinz.spinu@gmail.com>
2025-06-18 03:07:49 +0300

    all done

--- controlers/print.js
+++ controlers/print.js
@@ -1487,11 +1487,16 @@ function createRecipt(recipt, mode){
     doc.moveTo(400, height)
        .lineTo(400, height + 130)
        .stroke();
+    doc.lineWidth(0.8);
+        doc.moveTo(400, height + 80)
+        .lineTo(560, height + 80)
+        .stroke();
     doc.undash();
+    doc.image('public/icons/favicon.png', 450, height + 30, {width: 100})
     doc.fontSize(12)
     doc.font('public/font/Montserrat-Regular.ttf')
-    doc.text('Casier,', 415, height + 20)
-    doc.text('Semnătură', 415, height + 70)
+    // doc.text('Casier,', 415, height + 20)
+    doc.text('Semnătură', 415, height + 90)
 
 
   height = height + 260 - 120
@@ -1582,7 +1587,7 @@ function createRecipt(recipt, mode){
     doc.text('Semnătură', 415, height + 70)
 
 
-  height = height + 260
+  height = height + 260 -120
   doc.dash(2, { space: 2 });
   doc.lineWidth(0.8);
   doc.moveTo(20, height)
