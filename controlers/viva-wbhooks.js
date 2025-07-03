79a97aef69515cb3570742d8595639215e33d342
Alin Spinu <alinz.spinu@gmail.com>
2025-07-03 17:03:53 +0300

    all done

--- controlers/viva-wbhooks.js
+++ controlers/viva-wbhooks.js
@@ -5,23 +5,23 @@ const axios = require('axios');
 module.exports.transactionCreated = async (req, res) => {
     console.log(req.body)
 
-    const url = 'https://www.vivapayments.com/api/messages/config/token'
-    try {
-        const credentials = Buffer.from(`${process.env.VIVA_MERCHANT_ID}:${process.env.VIVA_APY_KEY}`).toString('base64');
-        const response = await axios.get(url, {
-          headers: {
-            'Authorization': `Basic ${credentials}`
-          }
-        });
-        console.log('Response data:', response.data);
-        res.status(200).json({message: response.data})
-      } catch (error) {
-        console.error('Error fetching token:', error.response ? error.response.data : error.message);
-        res.status(500).json({message: error})
-      }
+    // const url = 'https://www.vivapayments.com/api/messages/config/token'
+    // try {
+    //     const credentials = Buffer.from(`${process.env.VIVA_MERCHANT_ID}:${process.env.VIVA_APY_KEY}`).toString('base64');
+    //     const response = await axios.get(url, {
+    //       headers: {
+    //         'Authorization': `Basic ${credentials}`
+    //       }
+    //     });
+    //     console.log('Response data:', response.data);
+    //     res.status(200).json({message: response.data})
+    //   } catch (error) {
+    //     console.error('Error fetching token:', error.response ? error.response.data : error.message);
+    //     res.status(500).json({message: error})
+    //   }
 
 
-    // res.status(200).json({message: 'Request recived'})
+    res.status(200).json({Key: '9F11E6672096B03EC72519550A131B78765C3E09'})
 
 
 }
