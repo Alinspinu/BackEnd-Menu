44c6baee28c26396a959b7f73c398b853d53d8b0
Alin Spinu <alinz.spinu@gmail.com>
2025-07-04 16:10:12 +0300

    all done

--- controlers/back-office/cats.js
+++ controlers/back-office/cats.js
@@ -12,18 +12,18 @@ module.exports.sendCats = async (req, res, next) => {
                 { path: 'category' },
                 {
                     path: 'subProducts', select: '-saleLog',
-                    populate: [
-                        {path: 'product', select: '-saleLog'},
-                        {
-                            path: 'ings.ing', select: 'name um ings productIngredient qty', 
-                                populate: {
-                                    path: 'ings.ing', select: 'name um qty' 
-                                }
-                        }
-                    ],
+                    // populate: [
+                    //     {path: 'product', select: '-saleLog'},
+                    //     {
+                    //         path: 'ings.ing', select: 'name um ings productIngredient qty', 
+                    //             populate: {
+                    //                 path: 'ings.ing', select: 'name um qty' 
+                    //             }
+                    //     }
+                    // ],
                 },
-                { path: 'paring', populate: { path: 'category', select: 'name' } },
-                { path: 'ingredients.ingredient' },
+                // { path: 'paring', populate: { path: 'category', select: 'name' } },
+                // { path: 'ingredients.ingredient' },
                 { 
                     path: 'ings.ing', select: 'name qty um productIngredient ings',
                         populate: {
