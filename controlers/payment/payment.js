74523ffc46a14a4a3fd955f19d9e90c6897e285a
Alin Spinu <alinz.spinu@gmail.com>
2025-07-18 20:01:56 +0300

    all done

--- controlers/payment/payment.js
+++ controlers/payment/payment.js
@@ -190,7 +190,6 @@ module.exports.reports = async (req, res, next) => {
     try{
         const {value, serverId} = req.query;
     const server = await PrintServer.findById(serverId)
-        socket.emit('reports', JSON.stringify({value: value, serverKey: server.key}))
         socket.emit('reports', JSON.stringify({value: value, serverKey: server.key, address: server.fiscalPrinter.driverAddress}))
         res.status(200).json({message: 'Operatie efectuată cu success!'})
     } catch(err) {
@@ -202,7 +201,6 @@ module.exports.reports = async (req, res, next) => {
 module.exports.cashInandOut = async (req, res, next) =>{
     try{
         const {data, mainServer} = req.body;
-        socket.emit('inOut', JSON.stringify({data: data, serverKey: mainServer.key}))
         socket.emit('inOut', JSON.stringify({data: data, serverKey: mainServer.key, address: mainServer.fiscalPrinter.driverAddress}))
         res.status(200).json({message: 'Operatie efectuată cu success!'})
     } catch(err) {
@@ -216,7 +214,6 @@ module.exports.reprinFiscal = async (req, res, next) => {
         const {bill, mainServer} = req.body
         const newRep = new RepBill({fiscal: true, bill: bill._id})
         await newRep.save()
-        socket.emit('printBill', JSON.stringify({bill: bill, serverKey: mainServer.key}))
         socket.emit('printBill', JSON.stringify({bill: bill, serverKey: mainServer.key, address: mainServer.fiscalPrinter.driverAddress}))
         res.status(200).json({message: 'Bonul a fost retipărit!'})
      
@@ -234,7 +231,6 @@ module.exports.printBill = async (req, res, next) => {
         bill.pending = false
         const email = bill.clientInfo.email
         if(mode && bill.total > 0 && mainServer){
-           socket.emit('printBill', JSON.stringify({bill: bill, serverKey: mainServer.key}))
            socket.emit('printBill', JSON.stringify({bill: bill, serverKey: mainServer.key, address: mainServer.fiscalPrinter.driverAddress}))
         } 
         if(email && email.length){
@@ -287,7 +283,6 @@ module.exports.printUnreg = async (req, res, next) => {
         const billl = JSON.parse(bill)
         const newRep = new RepBill({fiscal: false, bill: billl._id})
         await newRep.save()
-        socket.emit('nefiscal', JSON.stringify({bill: billl, serverKey: mainServer.key}))
         socket.emit('nefiscal', JSON.stringify({bill: billl, serverKey: mainServer.key, address: mainServer.fiscalPrinter.driverAddress}))
         res.status(200).json({message: 'Bonul a fost tipărit!'})
     } catch(err){
