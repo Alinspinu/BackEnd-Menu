3639cd37a78833af119f7e12020cbe5f6b50c3e6
Alin Spinu <alinz.spinu@gmail.com>
2025-07-22 10:33:03 +0300

    all done

--- controlers/back-office/cash-register.js
+++ controlers/back-office/cash-register.js
@@ -275,14 +275,16 @@ module.exports.createXcel = async (req, res, next) => {
         worksheet.addRow(header)
         const entryArr = []
         days.forEach(el => {
-            el.entry.forEach(el => {
-                entryArr.push(el)
+            worksheet.addRow(['',`${el.date.toISOString().split('T')[0]}`,`Numerar din ziua precedentă`,`Intrare`,`${el.cashIn}`])
+            el.entry.forEach((el, i) => {
+                worksheet.addRow([`${i+1}`,`${el.date.toISOString().split('T')[0]}`,`${el.description}`,`${el.tip === 'income' ?'Intrare': 'Cheltuiala'}`,`${el.amount}`])
             })
+            worksheet.addRow(['',`${el.date.toISOString().split('T')[0]}`,`Numerar la sfârșit de zi`,`Ieșire`,`${el.cashOut}`])
         })
-        const sortedEntries = entryArr.sort((a,b) => b.index - a.index)
-        sortedEntries.forEach(el => {
-            worksheet.addRow([`${el.index}`,`${el.date.toISOString().split('T')[0]}`,`${el.description}`,`${el.tip === 'income' ?'Intrare': 'Cheltuiala'}`,`${el.amount}`])
-        })
+        // const sortedEntries = entryArr.sort((a,b) => b.index - a.index)
+        // sortedEntries.forEach(el => {
+        //     worksheet.addRow([`${el.index}`,`${el.date.toISOString().split('T')[0]}`,`${el.description}`,`${el.tip === 'income' ?'Intrare': 'Cheltuiala'}`,`${el.amount}`])
+        // })
         worksheet.addRow(footer)
         worksheet.addRow(inAndOut)
         worksheet.getRow(1).eachCell((cell)=>{
