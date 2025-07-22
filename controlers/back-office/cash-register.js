6000dec2a6fd07c67df9673111cc944d29ec278e
Alin Spinu <alinz.spinu@gmail.com>
2025-07-22 11:47:30 +0300

    all done

--- controlers/back-office/cash-register.js
+++ controlers/back-office/cash-register.js
@@ -262,10 +262,9 @@ module.exports.createXcel = async (req, res, next) => {
             el.entry.forEach((el, i) => {
                const e = worksheet.addRow([`${i+1}`,`${el.date.toISOString().split('T')[0]}`,`${el.description}`,`${el.tip === 'income' ?'Intrare': 'Ieșire'}`,`${el.amount}`])
                const sum = e.getCell(5)
-               console.log(sum)
                sum.font =  { color:  {argb: el.tip === 'income' ?  'FF00B050' : 'FF0000'} }
-               const tip = e.getCell(4)
-               tip.font = { color:  {argb: el.tip === 'income' ?  'FF00B050' : 'FF0000'} }
+            //    const tip = e.getCell(4)
+            //    tip.font = { color:  {argb: el.tip === 'income' ?  'FF00B050' : 'FF0000'} }
             });
          
            const foot =  worksheet.addRow(['',`${el.date.toISOString().split('T')[0]}`,`Numerar la sfârșit de zi`,`Ieșire`,`${round(el.cashOut)}`])
