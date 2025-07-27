51628a47e04667d4c6ce3203711f1c891c7f97a5
Alin Spinu <alinz.spinu@gmail.com>
2025-07-27 19:55:18 +0300

    all done

--- controlers/back-office/table.js
+++ controlers/back-office/table.js
@@ -13,6 +13,7 @@ module.exports.sendTables = async (req, res, next) => {
             match: {status: "open", locatie: loc, salePoint: point}, 
             populate: {path: 'masaRest', select: 'index'}
         })
+        console.log(tables)
         const sortedTables = tables.sort((a,b) => a.index - b.index)
         res.status(200).json(sortedTables)
     } catch(err){
