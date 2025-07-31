f7d7cb9b907e895c7ebab471655ab5b5ac33fe06
Alin Spinu <alinz.spinu@gmail.com>
2025-08-01 01:07:38 +0300

    all done

--- controlers/back-office/product.js
+++ controlers/back-office/product.js
@@ -16,28 +16,28 @@ const {checkTopping, round} = require('../../utils/functions')
 module.exports.changeVat = async (req, res) => {
     try{
 
-        const subP = await SubProduct.find({locatie: "655e2e7c5a3d53943c6b7c53"}).populate({path: 'product', select: 'name mainCat'})
+        // const subP = await SubProduct.find({locatie: "655e2e7c5a3d53943c6b7c53"}).populate({path: 'product', select: 'name mainCat'})
         const products = await Product.find({locatie: "655e2e7c5a3d53943c6b7c53"}).populate({path: 'category', select: 'name'})
         
-        for(let s of subP){
-            if(!s.product){
-                console.log(s.name)
-            } else {
-                if(s.product.mainCat === 'coffee'){
-                    s.price = s.price + 1
-                    console.log(s.product.name, ' ',  s.name, '---', s.price)
-                    await s.save()
-                }
-            }
-        }
-
-        // for(let p of products){
-        //     if(p.category.name === 'COCKTAILS' || p.mainCat === 'coffee'){
-        //         p.price = p.price + 1
-        //         console.log(p.name, '---', p.price)
-        //         await p.save()
+        // for(let s of subP){
+        //     if(!s.product){
+        //         console.log(s.name)
+        //     } else {
+        //         if(s.product.mainCat === 'coffee'){
+        //             s.price = s.price + 1
+        //             console.log(s.product.name, ' ',  s.name, '---', s.price)
+        //             await s.save()
+        //         }
         //     }
         // }
+
+        for(let p of products){
+            if(p.category.name === 'COCKTAILS' || p.mainCat === 'coffee'){
+                p.price = p.price + 1
+                console.log(p.name, '---', p.price)
+                await p.save()
+            }
+        }
         
         console.log('Numar de produse', products.length)
         console.log('Numar de sub', subP.length)
