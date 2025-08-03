884ba0809e6c0ac0c7b101803e6f14e362b206d7
Alin Spinu <alinz.spinu@gmail.com>
2025-08-03 16:04:18 +0300

    all done

--- controlers/back-office/invoice/controlers.js
+++ controlers/back-office/invoice/controlers.js
@@ -198,7 +198,7 @@ module.exports.handleUplodErros = async (req, res) => {
 
 module.exports.getMessages = async (req, res) => {
     const {days, filter = 'P', loc} = req.query
-
+    console.log('flitru****', filter)
     try{
     const locatie = await Locatie.findById(loc).populate({path: 'anafToken', select: 'token'})
     if(locatie){
