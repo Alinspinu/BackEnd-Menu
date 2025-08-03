b70f807a9a6629988ac08a33a82827f26298ccff
Alin Spinu <alinz.spinu@gmail.com>
2025-08-03 16:22:38 +0300

    all done

--- controlers/back-office/invoice/download-zip.js
+++ controlers/back-office/invoice/download-zip.js
@@ -14,8 +14,19 @@ const mode = 'test'
           'Accept': 'application/zip',
         },
       });
-      console.log(response.data)
-      // const zip = new AdmZip(response.data);
+      const asText = response.data.toString('utf-8');
+
+      // Is it a JSON error?
+      try {
+        const maybeJson = JSON.parse(asText);
+        if (maybeJson.eroare) {
+          throw new Error('Server error: ' + maybeJson.eroare);
+        }
+      } catch (e) {
+        // Not JSON → continue to ZIP
+      }
+      const zip = new AdmZip(response.data);
+
       const zipEntries = zip.getEntries(); 
       let invoice;
       for (const entry of zipEntries) {
