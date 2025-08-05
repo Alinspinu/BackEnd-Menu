3df5a6a4459d8d8730fc59eb92bf89173c0baeb9
Alin Spinu <alinz.spinu@gmail.com>
2025-08-05 19:55:59 +0300

    all done

--- controlers/back-office/report.js
+++ controlers/back-office/report.js
@@ -98,6 +98,9 @@ module.exports.getReportsDates = async (req, res) => {
         const {loc, point} = req.query
         const firstRep = await Report.find({locatie: loc, salePoint: point}).sort({day: 1}).limit(1)
         const lastRep = await Report.find({locatie: loc, salePoint: point}).sort({day: -1}).limit(1)
+        if(!firstRep){
+            return res.status(404).json({message: 'NU exista rapoarte salvate!'})
+        }
         const firstRepDate = firstRep[0].day
         const lastReportDate = lastRep[0].day
         res.status(200).json({start: firstRepDate, end: lastReportDate})
