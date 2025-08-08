50fb449c3a0a0129919e3f628af8eca83c226071
Alin Spinu <alinz.spinu@gmail.com>
2025-08-08 19:38:35 +0300

    sss

--- controlers/back-office/shedule.js
+++ controlers/back-office/shedule.js
@@ -178,13 +178,14 @@ module.exports.getShedules = async (req, res, next) => {
             .sort({_id: -1})
             .limit(3)
             .populate({path: 'days.users.employee', select: 'employee.fullName'})
-            // .populate({path: 'days.users.workPeriod.employeePosition'})
+            .populate({path: 'days.users.workPeriod.employeePosition'})
             const shedule = getNowShedule(shedules)
             res.status(200).json(shedule)
         }
         if(shedule === 'all'){
             const shedules = await Shedule.find({locatie: loc, salePoint: point})
                   .populate({path: 'days.users.employee', select: 'name employee.fullName'})
+                  .populate({path: 'days.users.workPeriod.employeePosition'})
           //  updateShedules(shedules, loc)
             res.status(200).json(shedules)
         }
