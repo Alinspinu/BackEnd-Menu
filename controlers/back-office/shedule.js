82dc0899a633bf4cef3828e2e11289b3f1780e5f
Alin Spinu <alinz.spinu@gmail.com>
2025-08-09 23:16:32 +0300

    all done

--- controlers/back-office/shedule.js
+++ controlers/back-office/shedule.js
@@ -178,8 +178,8 @@ module.exports.getPontaj = async (req, res, next) => {
                 .sort({_id: -1})
                 .limit(3)
                 .populate({path: 'days.users.employeePosition'})
-            // const pontaj = getNowShedule(pontajs)    
-             updateShedules(pontaj, loc)
+            const pontaj = getNowShedule(pontajs)    
+            //  updateShedules(pontaj, loc)
             res.status(200).json(pontaj)
         }
         if(pont === 'all'){
