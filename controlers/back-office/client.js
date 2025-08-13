3908d86b16993b60bf2b548c46bed425a380b66b
Alin Spinu <alinz.spinu@gmail.com>
2025-08-13 10:57:06 +0300

    all done

--- controlers/back-office/client.js
+++ controlers/back-office/client.js
@@ -8,6 +8,7 @@ module.exports.addClient = async (req, res) => {
 
         const newClient = new Client(client)
         const savedClient = await newClient.save()
+        console.log(savedClient)
         res.status(200).json({message: 'Clientul a fost salvat cu success!', client: savedClient})
     } catch(error) {
         console.log(error)
@@ -19,6 +20,8 @@ module.exports.getClients = async (req, res) => {
     const {loc} = req.query
     try{
         const clients = await Client.find({locatie: loc})
+        console.log(clients)
+        console.log(loc)
         res.status(200).json(clients)
     } catch(error) {
         console.log(error)
