6e711706f1c04e939301b8da7b5ae980d60a8ab0
Alin Spinu <alinz.spinu@gmail.com>
2025-08-13 09:46:42 +0300

    all done

--- controlers/back-office/info-cui.js
+++ controlers/back-office/info-cui.js
@@ -14,3 +14,20 @@ module.exports.getComapnyData = async (req, res) => {
         res.status(500).json(err)
     }
 }
+
+
+module.exports.getClientAddress = async (req, res) => {
+    const {city, street} = req.body
+    const country = 'Romania'
+    const cityU = encodeURIComponent(city)
+    const streetU = encodeURIComponent(street);
+    try{
+        const response = await axios.get(`${process.env.GEOAPY_URL}?country=${country}&city=${cityU}&street=${streetU}&apiKey=${process.env.GEOAPY_KEY}`)
+        res.status(200).json(response.data)
+    } catch(error){
+        console.log(error)
+        res.status(500).json(error)
+    }
+
+
+}
--- routes/back-office/info-cui.js
+++ routes/back-office/info-cui.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const infoCont = require('../../controlers/back-office/info-cui')
 
 
-router.route('/').get(infoCont.getComapnyData)
+router.route('/')
+    .get(infoCont.getComapnyData)
+    .post(infoCont.getClientAddress)
 
 
-module.exports = router
+module.exports = routerp
