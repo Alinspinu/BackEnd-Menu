2614ba2162d00b14090e4da00fd4d5d5c3a6db0f
Alin Spinu <alinz.spinu@gmail.com>
2025-08-18 16:00:33 +0300

    all done

--- controlers/back-office/invoice/parseXml.js
+++ controlers/back-office/invoice/parseXml.js
@@ -6,9 +6,9 @@ const { parseStringPromise } = require('xml2js');
   function parseInvoiceData (invoiceData, id) {
 
     const suplierIban = invoiceData.Invoice["cac:PaymentMeans"]?.[0]?.['cac:PayeeFinancialAccount']?.[0]
-    console.log(suplierIban)
-    const iban = suplierIban?.['cbc:ID'][0]
-    const bank = suplierIban?.['cbc:Name'][0]
+
+    const iban = suplierIban?.['cbc:ID'] ? suplierIban?.['cbc:ID'][0] : 'NO IBAN'
+    const bank = suplierIban?.['cbc:Name'] ? suplierIban?.['cbc:Name'][0] : 'NO NAME'
 
    const invoiceNumber = Array.isArray(invoiceData.Invoice["cbc:ID"]) 
     ? (invoiceData.Invoice["cbc:ID"][0]["_"] || invoiceData.Invoice["cbc:ID"][0]) 
