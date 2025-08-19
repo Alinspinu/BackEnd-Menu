e0f8f389ec2674604bcc472e74da3c493a8193af
Alin Spinu <alinz.spinu@gmail.com>
2025-08-19 15:29:01 +0300

    all done

--- controlers/back-office/product.js
+++ controlers/back-office/product.js
@@ -111,8 +111,8 @@ module.exports.updateProducts = async (req, res) => {
                 }
         },
     ])
-    
-       modifyProducts(products)
+
+    //    modifyProducts(products)
 
       const sortedProducts = products.sort((a, b) => a.name.localeCompare(b.name))
       res.status(200).json(sortedProducts)
