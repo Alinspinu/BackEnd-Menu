6066f78c017400d815ad90a24999ae4a10f08a31
Alin Spinu <alinz.spinu@gmail.com>
2025-08-20 10:30:41 +0300

    all done

--- controlers/back-office/invoice/parseXml.js
+++ controlers/back-office/invoice/parseXml.js
@@ -68,7 +68,7 @@ const { parseStringPromise } = require('xml2js');
         const price = item["cac:Price"] && item["cac:Price"][0]["cbc:PriceAmount"]
           ? +item["cac:Price"][0]["cbc:PriceAmount"][0]["_"] || +item["cac:Price"][0]["cbc:PriceAmount"][0]
           : 0;
-      
+      console.log(item)
         // Extract quantity and unit code
         const iq = item["cbc:InvoicedQuantity"]?.[0];
         const quantity = (typeof iq === 'object') ? +iq['_'] : +iq;
