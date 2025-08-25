5c1a4dc76a2d209db65218a8c83ef7ce838713c3
Alin Spinu <alinz.spinu@gmail.com>
2025-08-25 15:47:54 +0300

    all done

--- controlers/back-office/invetary.js
+++ controlers/back-office/invetary.js
@@ -200,7 +200,7 @@ module.exports.getComaredInv = async (req, res) => {
         const {point, loc, id} = req.query
         console.log(id)
         if(id){
-          const compInv =  await ComparedInventary.findById(id)
+          const compInv =  await ComparedInventary.findById(id).populate({path: 'gestiune', select: 'name'})
             res.status(200).json(compInv)
         } else {
             const compareInv = await ComparedInventary.find({locatie: loc, salePoint: point})
