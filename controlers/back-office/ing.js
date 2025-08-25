764464ad15a54299759b117a923ad2ae1d5d30e6
Alin Spinu <alinz.spinu@gmail.com>
2025-08-25 18:49:54 +0300

    all done

--- controlers/back-office/ing.js
+++ controlers/back-office/ing.js
@@ -59,7 +59,7 @@ module.exports.saveIng = async(req, res, next) => {
         const totalPages = Math.ceil(totalItems / limit);
 
 
-        // await modifyProducts(items)
+        await modifyProducts(items)
 
 
         res.status(200).json({
