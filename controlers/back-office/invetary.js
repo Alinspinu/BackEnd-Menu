bd415f3ebc36b4c39d744c5a34089b5c237c31e8
Alin Spinu <alinz.spinu@gmail.com>
2025-08-25 10:45:57 +0300

    all done

--- controlers/back-office/invetary.js
+++ controlers/back-office/invetary.js
@@ -198,7 +198,7 @@ module.exports.getComaredInv = async (req, res) => {
 
     try{
         const {point, loc} = req.query
-
+        console.log(point, loc)
         const compareInv = await ComparedInventary.find({locatie: loc, point: point})
         console.log(compareInv)
         res.status(200).json(compareInv)
