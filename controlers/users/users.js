61cef98e07d8b73c401adc65f033ad31b1b36dab
Alin Spinu <alinz.spinu@gmail.com>
2025-09-07 17:05:51 +0300

    all done

--- controlers/users/users.js
+++ controlers/users/users.js
@@ -32,7 +32,7 @@ module.exports.sendUsers = async (req, res, next) => {
 module.exports.sendEmployees = async (req, res) => {
     const {loc} = req.query
     try{
-        const user = await User.find({locatie: loc, client: false}).select('-password').populate({path: 'employee.employeePosition'});
+        const user = await User.find({locatie: loc, client: false, active: true}).select('-password').populate({path: 'employee.employeePosition'});
         const teo = user.find(u => u.employee.fullName === 'Teodora Lulciuc')
         console.log(teo)
         const sortedUsers = user.sort((a, b) => a.name.localeCompare(b.name));
@@ -125,7 +125,7 @@ module.exports.editPosition = async (req, res) => {
     const {id, update} = req.body
     try{
         const position  = await EmployeePosition.findByIdAndUpdate(update.employee.employeePosition._id, update.employee.employeePosition, {new: true})
-        const user = User.findById(id).populate({path: 'employee.employeePosition'})
+        const user = await User.findById(id).populate({path: 'employee.employeePosition'})
         res.status(200).json({message: 'Utilizatorul a fost actualizat!', user: user})
     } catch(error) {
         console.log(error)
