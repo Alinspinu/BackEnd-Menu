3dd1e8d27050ca63c878a06408c3d4bfe6594580
Alin Spinu <alinz.spinu@gmail.com>
2025-09-08 17:27:26 +0300

    all done

--- controlers/back-office/invetary.js
+++ controlers/back-office/invetary.js
@@ -268,13 +268,13 @@ module.exports.compareScriptic = async (req, res, next) => {
               for(let ingg of ing.ing.ings){
                   const index = delIngs.findIndex(i => i.name === ingg.ing.name)
                   if(index !== -1){
-                      if(ingg.name === 'Oua'){
+                      if(ingg.ing.name === 'Oua'){
                         console.log(ingg.qty)
                       }
                       delIngs[index].qty = round(delIngs[index].qty + ingg.qty)
                   } else {
                      if(ingg.gestiune.toString() === firstInventary.gestiune.toString() ) {
-                      if(ingg.name === 'Oua'){
+                      if(ingg.ing.name === 'Oua'){
                         console.log(ingg.qty)
                       }
                       delIngs.push(ingg)
@@ -284,13 +284,13 @@ module.exports.compareScriptic = async (req, res, next) => {
           } else {
               const index = delIngs.findIndex(i => i.name === ing.ing.name)
               if(index !== -1){
-                if(ing.name === 'Oua'){
+                if(ing.ing.name === 'Oua'){
                   console.log(ing.qty)
                 }
                   delIngs[index].qty = round(delIngs[index].qty + ing.qty)
               } else {
                 if(ing.gestiune.toString() === firstInventary.gestiune.toString()) {
-                  if(ing.name === 'Oua'){
+                  if(ing.ing.name === 'Oua'){
                     console.log(ing.qty)
                   }
                   delIngs.push(ing)
@@ -518,6 +518,9 @@ module.exports.compareScriptic = async (req, res, next) => {
           value: 0,
           entries: []
         },
+      }
+      if(ing.ing?.name === 'Oua'){
+        console.log('oua produs sters', ing.qty)
       }
       const existingIng = ingredients.find(ingd => ingd.name === compareIng.name)
       if(existingIng){
