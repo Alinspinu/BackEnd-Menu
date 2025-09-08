d891b877fae589dcb8409df9c3b99192fb5c7711
Alin Spinu <alinz.spinu@gmail.com>
2025-09-08 21:54:29 +0300

    all done

--- controlers/back-office/invetary.js
+++ controlers/back-office/invetary.js
@@ -707,7 +707,10 @@ module.exports.compareScriptic = async (req, res) => {
 
         for (const w of prod.ings || []) {
           const scaled = { ...w, qty: r((w.qty || 0) * mult) };
-          console.log(w)
+          if(scaled.name === 'Oua'){
+            console.log(scaled)
+          }
+          // console.log(w)
           if (scaled?.ing?.ings?.length) {
             for (const sub of scaled.ing.ings) {
               if (!sub?.ing?._id || !gestMatch(sub)) continue;
