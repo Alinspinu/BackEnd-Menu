8f61c4c86501f79c35553533d52d51d6cf0979be
Alin Spinu <alinz.spinu@gmail.com>
2025-09-08 19:31:51 +0300

    all done

--- controlers/back-office/invetary.js
+++ controlers/back-office/invetary.js
@@ -278,9 +278,6 @@ module.exports.compareScriptic = async (req, res, next) => {
           } else {
               const index = delIngs.findIndex(i => i.ing._id.toString() === ing.ing._id.toString())
               if(index !== -1){
-                if(ing.ing.name === 'Oua'){
-                  console.log(ing.qty)
-                }
                   delIngs[index].qty = round(delIngs[index].qty + ing.qty)
               } else {
                 if(ing.gestiune.toString() === firstInventary.gestiune.toString()) {
@@ -303,26 +300,26 @@ module.exports.compareScriptic = async (req, res, next) => {
         product.ings.forEach(ing => {
           if(ing.ing.ings && ing.ing.ings.length){
             ing.ing.ings.forEach(ig => {
-              const existingIng = delIngs.find(i => i.ing.name === ig.ing.name)
+              const existingIng = delIngs.find(i => i.ing._id.toString() === ig.ing._id.toString())
               if(existingIng){
                 const updatedIng = {
                   qty: round(existingIng.qty + (ig.qty * ing.qty)),
                   ing: existingIng.ing
                 }
-                delIngs = delIngs.map(p => (p.ing.name === ig.ing.name ? updatedIng : p));
+                delIngs = delIngs.map(p => (p.ing._id.toString() === ig.ing._id.toString() ? updatedIng : p));
               } else{
                 if(ig.gestiune.toString() === firstInventary.gestiune.toString() )  delIngs.push(ig)
               }
             })
           } else{
             if(ing && ing.ing){
-              const existingIngredient = delIngs.find(p =>p.ing.name === ing.ing.name);
+              const existingIngredient = delIngs.find(p =>p.ing._id.toString() === ing.ing._id.toString());
               if (existingIngredient) {
                 const updatedIng = {
                   qty: existingIngredient.qty + ing.qty,
                   ing: existingIngredient.ing
                 }
-                delIngs = delIngs.map(p => (p.ing.name === ing.ing.name ? updatedIng : p));
+                delIngs = delIngs.map(p => (p.ing._id.toString() === ing.ing._id.toString() ? updatedIng : p));
               } else {
                 if(!ing.gestiune){
                     console.log(ing)
@@ -336,26 +333,26 @@ module.exports.compareScriptic = async (req, res, next) => {
         product.toppings.forEach(ing => {
           if(ing.ing.ings && ing.ing.ings.length){
             ing.ing.ings.forEach(ig => {
-              const existingIng = delIngs.find(i => i.ing.name === ig.ing.name)
+              const existingIng = delIngs.find(i => i.ing._id.toString() === ig.ing._id.toString())
               if(existingIng){
                 const updatedIng = {
                   qty: existingIng.qty + round(ig.qty *ing.qty),
                   ing: existingIng.ing
                 }
-                delIngs = delIngs.map(p => (p.ing.name === ig.ing.name ? updatedIng : p));
+                delIngs = delIngs.map(p => (p.ing._id.toString() === ig.ing._id.toString() ? updatedIng : p));
               } else{
                 if(ig.gestiune.toString() === firstInventary.gestiune.toString() )  delIngs.push(ig)
               }
             })
           } else{
             if(ing && ing.ing){
-              const existingIngredient = delIngs.find(p =>p.ing.name === ing.ing.name);
+              const existingIngredient = delIngs.find(p =>p.ing._id.toString() === ing.ing._id.toString());
               if (existingIngredient) {
                 const updatedIng = {
                   qty: existingIngredient.qty + ing.qty,
                   ing: existingIngredient.ing
                 }
-                delIngs = delIngs.map(p => (p.ing.name === ing.ing.name ? updatedIng : p));
+                delIngs = delIngs.map(p => (p.ing._id.toString() === ing.ing._id.toString() ? updatedIng : p));
               } else {
                 if(ing.gestiune.toString() === firstInventary.gestiune.toString() )  delIngs.push(ing);
               }
@@ -508,9 +505,6 @@ module.exports.compareScriptic = async (req, res, next) => {
 
 
     delIngs.forEach(ing => {
-      // if(ing.ing.name === 'Oua'){
-      //   console.log(ing)
-      // }
       const compareIng = {
         name: ing.ing?.name,
         um: ing.ing?.um,
