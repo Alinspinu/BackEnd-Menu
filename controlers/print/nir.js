466a8082f27e896b0fdaef844ce44de0618f853e
Alin Spinu <alinz.spinu@gmail.com>
2025-09-12 17:20:25 +0300

    all done

--- controlers/print/nir.js
+++ controlers/print/nir.js
@@ -339,7 +339,7 @@ function addIngredients(doc, ingredients, lineHeigth, y, page, pageLenght, valTo
     const result = [];
     const l = arr.length
     if(l < 26 && l > 18) firstChunkSize = 18
-    if(l < 60 && l > 45) otherChunkSize = 19
+    if(l < 60 && l > 45) otherChunkSize = 22
     // First chunk
     result.push(arr.slice(0, firstChunkSize));
   
