28f11bd4b319d31427448cb6957a5788a2b44fa4
Alin Spinu <alinz.spinu@gmail.com>
2025-09-13 11:42:10 +0300

    all done

--- controlers/print/nir.js
+++ controlers/print/nir.js
@@ -373,12 +373,12 @@ function addIngredients(doc, ingredients, lineHeigth, y, page, pageLenght, valTo
 
           doc
           .moveTo(645.5, y + height + 59)
-          .lineTo(645.5, y + height + 59 + 26 + (vatValues.length * 11))
+          .lineTo(645.5, y + height + 45 + 26 + (vatValues.length * 11))
           .stroke();
 
           doc
           .moveTo(770.5, y + height + 59)
-          .lineTo(770.5, y + height + 59 + 26 + (vatValues.length * 11))
+          .lineTo(770.5, y + height + 45 + 26 + (vatValues.length * 11))
           .stroke();
 
           doc
