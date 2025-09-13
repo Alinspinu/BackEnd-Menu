ae13c30343a618af41fe9972eade9d2cd952d28d
Alin Spinu <alinz.spinu@gmail.com>
2025-09-13 14:10:42 +0300

    all done

--- controlers/back-office/ing.js
+++ controlers/back-office/ing.js
@@ -74,7 +74,7 @@ module.exports.saveIng = async(req, res, next) => {
 
     function verifyIngredients(ings){
       ings.forEach(i => {
-        const gest = i.invGestiune.find( g => g.gestiune.toString() === i.gest._id.toString())
+        const gest = i.invGestiune.find( g => g.gestiune.toString() === i.gest?._id.toString())
         if(gest.entries.length){
           const oldestEntry = gest.entries.reduce((oldest, current) => {
             return new Date(current.date).getTime() < new Date(oldest.date).getTime() ? current : oldest;
