dc3a7cfd66a53ee9bc8772f78bbb76f7704177ec
Alin Spinu <alinz.spinu@gmail.com>
2025-09-19 12:31:10 +0300

    all done

--- controlers/users/auth.js
+++ controlers/users/auth.js
@@ -616,7 +616,7 @@ module.exports.sendLogs =  async  (req, res) => {
 
 module.exports.handleAnafTokens = async (req, res) => {
         const { code, error } = req.query;
-        const baseRedirectUrl = 'http://front.flowmanager.ro/config/efactura'
+        const baseRedirectUrl = 'https://front.flowmanager.ro/config/efactura'
         console.log(req.query)
         if(error) {
             const redirectUrl = `${baseRedirectUrl}?error=${error}`
