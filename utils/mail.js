f92772435c9c7d02078ab958c30c8f2ce7d4d05a
Alin Spinu <alinz.spinu@gmail.com>
2025-09-23 22:44:43 +0300

    all done

--- utils/mail.js
+++ utils/mail.js
@@ -85,7 +85,8 @@ async function sendVerificationEmail(newUser) {
     const templateData = {
         otp: newUser.otp,
         name: newUser.name,
-        locatie: newUser.locatie.name
+        locatie: newUser.locatie.name,
+        logourl: newUser.locatie.name === 'T ZERO' ? 'https://res.cloudinary.com/dhetxk68c/image/upload/v1758656623/t_tyszya.svg' : 'https://res.cloudinary.com/dhetxk68c/image/upload/v1745824224/logo-true/logo-true-group_hxwb9h.svg'
     };
     const renderedTemplate = ejs.render(templateSource, {data: templateData});
     
