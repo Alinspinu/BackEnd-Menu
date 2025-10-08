e92e85db2a8a5b2401bc261b842a9b5b5e26caee
Alin Spinu <alinz.spinu@gmail.com>
2025-10-08 09:32:03 +0300

    all done

--- controlers/back-office/invoice/download-zip.js
+++ controlers/back-office/invoice/download-zip.js
@@ -32,9 +32,10 @@ const mode = 'prod'
       for (const entry of zipEntries) {
           if (!entry.entryName.includes('semnatura')) {
           const xmlData = entry.getData().toString('utf8'); 
-          console.log(xmlData)
+          const modifyXml = xmlData.replace(/n2:/g, 'cac:').replace(/n3:/g, 'cbc:').replace(/n1:/g, 'ext:');
+          console.log(modifyXml)
           try {
-              const result = await parseXml(xmlData); 
+              const result = await parseXml(modifyXml); 
               if(result.Invoice){
                 invoice = parseInvoiceData(result, id);
               } 
