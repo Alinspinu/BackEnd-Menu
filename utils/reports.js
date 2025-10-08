c9d8b75afd210c1616d8db892c4ecfec809a81ba
Alin Spinu <alinz.spinu@gmail.com>
2025-10-08 11:43:34 +0300

    all done

--- utils/reports.js
+++ utils/reports.js
@@ -758,7 +758,7 @@ async function createDayReport(billProducts, ingredients, loc, bills, dat, point
         if(ing.uploadLog){
             for(const log of ing.uploadLog) {
                     const logDate = new Date(log.date).getTime()
-                    if(startTime < logDate && logDate < endTime) {
+                    if(startTime <= logDate && logDate < endTime) {
                         switch (ing.dept.name) {
                             case 'Consumabil':                       
                                 if(!log.uploadPrice){
