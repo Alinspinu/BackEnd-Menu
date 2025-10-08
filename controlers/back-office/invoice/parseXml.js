6bb18602678b6414d6ec5d7dc2b96d981d58396f
Alin Spinu <alinz.spinu@gmail.com>
2025-10-08 09:21:40 +0300

    all done

--- controlers/back-office/invoice/parseXml.js
+++ controlers/back-office/invoice/parseXml.js
@@ -52,7 +52,7 @@ const { parseStringPromise } = require('xml2js');
         : 'Unknown VAT Number'
     };
 
-    console.log(invoiceData.invoice)
+    console.log(invoiceData.Invoice)
 
     const products = invoiceData.Invoice["cac:InvoiceLine"].map(item => {
       // ------- helpers -------
