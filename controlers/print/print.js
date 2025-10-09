8ecc7db40814f9243caf6fb4353dace0c5f95f0e
Alin Spinu <alinz.spinu@gmail.com>
2025-10-09 18:38:50 +0300

    all done

--- controlers/print/print.js
+++ controlers/print/print.js
@@ -828,7 +828,7 @@ module.exports.printConsum = async (req, res) => {
               }
             })
             if(product.toppings.length){
-              console.log(product.toppings.name)
+              console.log(product.toppings.ing.name)
               product.toppings.forEach(topping=>{
                 if(topping.ing.ings.length){
                   topping.ing.ings.forEach(ig => {
