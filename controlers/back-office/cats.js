4f3cdb154253f09a153eb83f0dc7f534aebab915
Alin Spinu <alinz.spinu@gmail.com>
2025-10-13 14:26:13 +0200

    all done

--- controlers/back-office/cats.js
+++ controlers/back-office/cats.js
@@ -40,41 +40,10 @@ module.exports.sendCats = async (req, res, next) => {
         })
         .lean({ virtuals: false })
         .maxTimeMS(20000);
-
-       await modyfySubProducts(cats)
-
         res.status(200).json(cats);
     } catch (err) {
         console.log(err)
         res.status(500).json({ message: err.error?.message })
-    }
-}
-
-
-async function modyfySubProducts(cats){
-    for(let cat of cats) {
-        for(let p of cat.product){
-
-            for(let i of p.ings){
-                     if(!i.gestiune){
-                        console.log('produs gasit fara gestiune la ingredient', p.name)
-                        i.gestiune = i.ing.invGestiune[0].gestiune
-                        console.log('gestiune adaugata ', i.gestiune)
-                        // await s.save()
-                    }
-                }
-
-            // for(let s of p.subProducts){
-            //     for(let i of s.ings){
-            //         if(!i.gestiune){
-            //             console.log('sub produs gasit fara gestiune la ingredient', s.name)
-            //             i.gestiune = i.ing.invGestiune[0].gestiune
-            //             console.log('gestiune adaugata ', i.gestiune)
-            //             // await s.save()
-            //         }
-            //     }
-            // }
-        }
     }
 }
 
