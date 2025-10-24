893cb5ec50c1202a4d0826feaea3ebced09422cb
Alin Spinu <alinz.spinu@gmail.com>
2025-10-24 16:40:31 +0300

    all done

--- utils/reports.js
+++ utils/reports.js
@@ -1079,7 +1079,7 @@ async function createDayReport(billProducts, ingredients, loc, bills, dat, point
     })
     const newRep = await report.save()
     // console.log(values)
-    console.log(newRep.departaments)
+    // console.log(newRep.departaments)
     // console.log(newRep.departaments)
     // console.log(values)
     return newRep
