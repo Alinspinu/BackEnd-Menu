346884458100a38e6b8db26ecf089b975890865a
Alin Spinu <alinz.spinu@gmail.com>
2025-10-24 16:10:51 +0300

    all done

--- utils/reports.js
+++ utils/reports.js
@@ -590,8 +590,9 @@ async function createDayReport(billProducts, ingredients, loc, bills, dat, point
                 if(g){
                     g.totalIn += e.price
                     g.entries.push(e)
-                 const de = g.dep.find(dp => dp.depId === d.dep.toString())
+                 const de = g.dep.find(dp => dp.depId.toString() === d.dep.toString())
                  if(de){
+                    console.log('hit')
                     de.totalIn = d.total
                  }
                 }
