d1d44be700be79e2aeacfb62940856712e7f2618
Alin Spinu <alinz.spinu@gmail.com>
2025-10-25 15:52:57 +0300

    all done

--- controlers/print/invoice.js
+++ controlers/print/invoice.js
@@ -131,6 +131,7 @@ let rowHeigth = 12
 
 let firstChunk = 0
 let restChunks = 0
+let secondIndex = 30
 
 if(productsCount <= 30){
   firstChunk = 28
@@ -142,6 +143,7 @@ if(productsCount > 30 && productsCount <= 41){
 }
 
 if(productsCount > 40){
+  secondIndex = 41
   firstChunk = 41
   restChunks = 54
 }
@@ -162,7 +164,7 @@ if(ingChunks[ingChunks.length -1].length > firstChunk || (ingChunks[ingChunks.le
 }
 
 ingChunks.forEach((ch, i) => {
-    doc = addIngredients(doc, ch, rowHeigth, y, i+1, headerHeight, rectHeigth, rectStartH, heghtValue, pages, invoice.taxExclusiveAmount, invoice.taxInclusiveAmount, invoice.vatAmount, pageCount)
+    doc = addIngredients(doc, ch, rowHeigth, y, i+1, headerHeight, rectHeigth, rectStartH, heghtValue, pages, invoice.taxExclusiveAmount, invoice.taxInclusiveAmount, invoice.vatAmount, pageCount, secondIndex)
 })
 
 //header produsex
@@ -373,7 +375,7 @@ return doc
 }
 
 
-function addIngredients(doc, ch, rowHeigth, y, page, headerHeight, rectHeigth, rectStartH, heghtValue, pageLenght, taxExclusiveAmount, taxInclusiveAmount, vatAmount, pageCount){
+function addIngredients(doc, ch, rowHeigth, y, page, headerHeight, rectHeigth, rectStartH, heghtValue, pageLenght, taxExclusiveAmount, taxInclusiveAmount, vatAmount, pageCount, secondIndex){
   doc.fillColor('black') 
 
 
@@ -385,7 +387,7 @@ function addIngredients(doc, ch, rowHeigth, y, page, headerHeight, rectHeigth, r
      rectHeigth = rectHeigth - 150
   }
   let index = 1
-  if(page === 2) index = 1 + 41
+  if(page === 2) index = 1 + secondIndex
   if(page === 3) index = 41 + 54
   if(page === 4) index = 41 + 54 + 54
   if(page === 5) index = 41 + 54 + 54 + 54
