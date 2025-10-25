24b57967637646a6202162c0e01f7b4d043796f2
Alin Spinu <alinz.spinu@gmail.com>
2025-10-25 15:43:39 +0300

    all done

--- controlers/print/invoice.js
+++ controlers/print/invoice.js
@@ -117,11 +117,11 @@ doc.text(`${invoice.dueDate}`, 275, 190 + 15)
 
 
 
-let rectStartH = 247
-let rectHeigth = 650
-let headerHeight = 200
+let rectStartH = 247 - 50
+let rectHeigth = 650 - 50
+let headerHeight = 200 - 50
 // radare produse
-y = 237
+y = 237 - 50
 let heghtValue = 12
 
 // doc.font("Courier");
