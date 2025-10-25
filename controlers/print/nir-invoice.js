ae03200eb7f231989a90d2e57c42e16f2ed02e9a
Alin Spinu <alinz.spinu@gmail.com>
2025-10-25 13:57:38 +0300

    all done

--- controlers/print/nir-invoice.js
+++ controlers/print/nir-invoice.js
@@ -88,101 +88,10 @@ doc.text(`${invoice.invoiceNumber}`, 275, 190 + 35)
 doc.text(`${invoice.issueDate}`, 275, 205 + 35)
 doc.text(`${invoice.dueDate}`, 275, 220 + 35)
 
-//header produsex
-doc.rect(25, 280, 18, 30)
-doc.lineWidth(0.5);
-doc.stroke()
 
-doc.font("public/font/Montserrat-Regular.ttf");
-doc.fontSize(9)
-doc.text('Nr.', 26, 281)
-doc.text('crt.', 26, 296)
-
-doc.rect(43, 280, 230, 30)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('Denumirea produselor si serviciilor', 44, 293, { width: 228, align: "center" })
-
-doc.rect(273, 280, 30, 30)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('U.M.', 275, 293, { width: 28, align: "center" })
-
-doc.rect(303, 280, 60, 30)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('Cantitate', 305, 293, { width: 58, align: "center" })
-
-doc.rect(363, 280, 60, 30)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('Pret unitar', 365, 287, { width: 58, align: "center" })
-doc.text('fara T.V.A.', 365, 299, { width: 58, align: "center" })
-
-doc.rect(423, 280, 60, 30)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('Valoare', 425, 287, { width: 58, align: "center" })
-doc.text('fara T.V.A.', 425, 299, { width: 58, align: "center" })
-
-doc.rect(483, 280, 77, 30)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('T.V.A.', 485, 285, { width: 75, align: "center" })
-doc.text('Cota', 485, 299, { width: 35, align: "left" })
-doc.text('Valoare', 521, 299, { width: 38, align: "right" })
-
-//little header
-doc.rect(25, 311, 18, 15)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('0', 26, 315, { width: 17, align: "center" })
-
-doc.rect(43, 311, 230, 15)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('1', 44, 315, { width: 228, align: "center" })
-
-doc.rect(273, 311, 30, 15)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('2', 274, 315, { width: 28, align: "center" })
-
-doc.rect(303, 311, 60, 15)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('3', 304, 315, { width: 58, align: "center" })
-
-doc.rect(363, 311, 60, 15)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('4', 364, 315, { width: 58, align: "center" })
-
-doc.rect(423, 311, 60, 15)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('5 = 3 x 4', 424, 315, { width: 58, align: "center" })
-
-doc.rect(483, 311, 77, 15)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.text('6', 484, 315, { width: 75, align: "center" })
-
-
-
+let rectStartH = 327
+let rectHeigth = 330
+let headerHeight = 280
 // radare produse
 y = 317
 let heghtValue = 12
@@ -203,93 +112,248 @@ if(productsCount > 50){
   doc.fontSize(5)
   rowHeigth = 5
 }
+if(productsCount > 60){
+  rectHeigth
+}
 
 
+ 
 
-//Body produse
-doc.rect(25, 327, 18, 330)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.rect(43, 327, 230, 330)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.rect(273, 327, 30, 330)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.rect(303, 327, 60, 330)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.rect(363, 327, 60, 330)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.rect(423, 327, 60, 330)
-doc.lineWidth(0.5);
-doc.stroke()
-
-doc.rect(483, 327, 77, 330)
-doc.lineWidth(0.5);
-doc.stroke()
+const ingChunks = splitIngredients(invoice.products)
 
 
+let pages = ingChunks.length
 
+let pageCount = ingChunks.length
+if(ingChunks[ingChunks.length -1].length > 40 || (ingChunks[ingChunks.length -1].length > 36) && ingChunks.length === 1 ) {
+    pageCount = pageCount + 1
+}
 
-
-
-// doc.fontSize(8)
-invoice.products.forEach((el, i) => {
-    let newValue = y + heghtValue
-    doc.text(`${i + 1}`, 26, newValue, { width: 17, align: "center" })
-    doc.text(`${el.name}`, 47, newValue, { width: 225, align: 'left' })
-    doc.text(`Buc`, 274, newValue, { width: 28, align: "center" })
-    doc.text(`${el.quantity}.00`, 304, newValue, { width: 58, align: "center" })
-    doc.text(`${round(el.totalNoVat/el.quantity)}`, 364, newValue, { width: 58, align: "center" })
-    doc.text(`${round(el.totalNoVat)}`, 424, newValue, { width: 58, align: "center" })
-    doc.text(`${el.vatPrecent}%`, 486, newValue, { width: 35, align: "left" })
-    doc.text(`${round((el.price * el.quantity) * (el.vatPrecent / 100))}`, 498, newValue, { width: 60, align: "right" })
-    if(value === el.totalNoVat){
-      doc.lineWidth(0.2);
-      doc.strokeColor('red');
-      doc.moveTo(26, newValue + rowHeigth -1).lineTo(272, newValue + rowHeigth -1).stroke();
-      doc.strokeColor('black');
-    }
-    heghtValue += rowHeigth
+ingChunks.forEach((ch, i) => {
+    doc = addIngredients(doc, ch, rowHeigth, y, i+1, headerHeight, rectHeigth, rectStartH, pages, invoice.taxExclusiveAmount, invoice.taxInclusiveAmount, invoice.vatAmount, pageCount)
 })
 
 
 
-
-
-
-
-doc.fontSize(10)
-//footer factura
-doc.rect(25, 669, 338, 105)
-doc.lineWidth(0.5);
-doc.stroke()
-// doc.image('public/icons/logo-stanga.png', 35, 689, {width: 300})
-
-doc.rect(363, 669, 197, 105)
-doc.lineWidth(0.5);
-doc.stroke()
-doc.fontSize(7)
-doc.text('(Total fără T.V.A.)', 380, 692, { width: 60, align: 'center' })
-doc.text('(Total T.V.A.)', 488, 692, { width: 60, align: 'center' })
-doc.font('public/font/Montserrat-Bold.ttf')
-doc.fontSize(12)
-doc.text(`${round(invoice.taxExclusiveAmount)} Lei`, 365, 678, { width: 90, align: 'center' })
-doc.text(`${round(invoice.vatAmount)} Lei`, 480, 678, { width: 75, align: 'center' })
-
-doc.fontSize(16)
-doc.text('TOTAL', 382, 745)
-doc.text(`${round(invoice.taxInclusiveAmount)} Lei`, 445, 745,{ width: 150, align: 'left' })
-
 return doc
 
 }
+
+
+
+function addIngredients(doc, ch, rowHeigth, y, headerHeight, rectHeigth, rectStartH, page, pageLenght, taxExclusiveAmount, taxInclusiveAmount, vatAmount, pageCount){
+     doc.fillColor('black') 
+  if(page !== 1) {
+      y = y - 180
+      headerHeight = headerHeight - 180
+      rectHeigth = rectHeigth - 180
+      rectStartH = rectStartH - 180
+  } 
+
+    //header produsex
+    doc.rect(25, headerHeight, 18, 30)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.font("public/font/Montserrat-Regular.ttf");
+    doc.fontSize(9)
+    doc.text('Nr.', 26, headerHeight + 1)
+    doc.text('crt.', 26, headerHeight + 16)
+
+    doc.rect(43, headerHeight, 230, 30)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('Denumirea produselor si serviciilor', 44, 293, { width: 228, align: "center" })
+
+    doc.rect(273, headerHeight, 30, 30)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('U.M.', 275, headerHeight + 13, { width: 28, align: "center" })
+
+    doc.rect(303, headerHeight, 60, 30)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('Cantitate', 305, headerHeight + 13, { width: 58, align: "center" })
+
+    doc.rect(363, headerHeight, 60, 30)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('Pret unitar', 365, headerHeight + 7, { width: 58, align: "center" })
+    doc.text('fara T.V.A.', 365, headerHeight + 19, { width: 58, align: "center" })
+
+    doc.rect(423, headerHeight, 60, 30)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('Valoare', 425, headerHeight + 7, { width: 58, align: "center" })
+    doc.text('fara T.V.A.', 425, headerHeight + 19, { width: 58, align: "center" })
+
+    doc.rect(483, headerHeight, 77, 30)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('T.V.A.', 485, headerHeight + 5, { width: 75, align: "center" })
+    doc.text('Cota', 485, headerHeight + 19, { width: 35, align: "left" })
+    doc.text('Valoare', 521, headerHeight + 19, { width: 38, align: "right" })
+
+    //little header
+    doc.rect(25, headerHeight + 31, 18, 15)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('0', 26, headerHeight + 35, { width: 17, align: "center" })
+
+    doc.rect(43, headerHeight + 31, 230, 15)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('1', 44, headerHeight + 35, { width: 228, align: "center" })
+
+    doc.rect(273, headerHeight + 31, 30, 15)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('2', 274, headerHeight + 35, { width: 28, align: "center" })
+
+    doc.rect(303, headerHeight + 31, 60, 15)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('3', 304, headerHeight + 35, { width: 58, align: "center" })
+
+    doc.rect(363, headerHeight + 31, 60, 15)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('4', 364, headerHeight + 35, { width: 58, align: "center" })
+
+    doc.rect(423, headerHeight + 31, 60, 15)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('5 = 3 x 4', 424, headerHeight + 35, { width: 58, align: "center" })
+
+    doc.rect(483, headerHeight + 31, 77, 15)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.text('6', 484, headerHeight + 35, { width: 75, align: "center" })
+
+
+
+
+
+    //Body produse
+    doc.rect(25, rectStartH, 18, rectHeigth)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.rect(43, rectStartH, 230, rectHeigth)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.rect(273, rectStartH, 30, rectHeigth)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.rect(303, rectStartH, 60, rectHeigth)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.rect(363, rectStartH, 60, rectHeigth)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.rect(423, rectStartH, 60, rectHeigth)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+    doc.rect(483, rectStartH, 77, rectHeigth)
+    doc.lineWidth(0.5);
+    doc.stroke()
+
+
+
+
+
+
+    // doc.fontSize(8)
+    invoice.products.forEach((el, i) => {
+        let newValue = y + heghtValue
+        doc.text(`${i + 1}`, 26, newValue, { width: 17, align: "center" })
+        doc.text(`${el.name}`, 47, newValue, { width: 225, align: 'left' })
+        doc.text(`Buc`, 274, newValue, { width: 28, align: "center" })
+        doc.text(`${el.quantity}.00`, 304, newValue, { width: 58, align: "center" })
+        doc.text(`${round(el.totalNoVat/el.quantity)}`, 364, newValue, { width: 58, align: "center" })
+        doc.text(`${round(el.totalNoVat)}`, 424, newValue, { width: 58, align: "center" })
+        doc.text(`${el.vatPrecent}%`, 486, newValue, { width: 35, align: "left" })
+        doc.text(`${round((el.price * el.quantity) * (el.vatPrecent / 100))}`, 498, newValue, { width: 60, align: "right" })
+        if(value === el.totalNoVat){
+          doc.lineWidth(0.2);
+          doc.strokeColor('red');
+          doc.moveTo(26, newValue + rowHeigth -1).lineTo(272, newValue + rowHeigth -1).stroke();
+          doc.strokeColor('black');
+        }
+        heghtValue += rowHeigth
+    })
+    const pageWidth = doc.page.width;
+    const pageHeight = doc.page.height;
+
+    if(page < pageLenght){
+      doc.fontSize(7)
+        .fillColor('gray')
+        .text(`Pagina ${page} din ${pageCount}`, pageWidth / 2 - 40, pageHeight - 90);
+      doc.addPage()
+  } else { 
+
+    doc.fontSize(10)
+    //footer factura
+    doc.rect(25, 669, 338, 105)
+    doc.lineWidth(0.5);
+    doc.stroke()
+    // doc.image('public/icons/logo-stanga.png', 35, 689, {width: 300})
+
+    doc.rect(363, 669, 197, 105)
+    doc.lineWidth(0.5);
+    doc.stroke()
+    doc.fontSize(7)
+    doc.text('(Total fără T.V.A.)', 380, 692, { width: 60, align: 'center' })
+    doc.text('(Total T.V.A.)', 488, 692, { width: 60, align: 'center' })
+    doc.font('public/font/Montserrat-Bold.ttf')
+    doc.fontSize(12)
+    doc.text(`${round(taxExclusiveAmount)} Lei`, 365, 678, { width: 90, align: 'center' })
+    doc.text(`${round(vatAmount)} Lei`, 480, 678, { width: 75, align: 'center' })
+
+    doc.fontSize(16)
+    doc.text('TOTAL', 382, 745)
+    doc.text(`${round(taxInclusiveAmount)} Lei`, 445, 745,{ width: 150, align: 'left' })
+
+  }
+
+
+  return doc
+}
+
+
+
+function splitIngredients(arr, firstChunkSize = 40, otherChunkSize = 40) {
+  const result = [];
+
+  // First chunk
+  result.push(arr.slice(0, firstChunkSize));
+
+  // Remaining chunks
+  for (let i = firstChunkSize; i < arr.length; i += otherChunkSize) {
+    result.push(arr.slice(i, i + otherChunkSize));
+  }
+
+  return result;
+}
+
+
 
 module.exports = {createNirInvoice}
