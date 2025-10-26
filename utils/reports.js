1cef44fc4540039e2afcee9940d6cde9e7d3c055
Alin Spinu <alinz.spinu@gmail.com>
2025-10-26 20:52:56 +0200

    all done

--- utils/reports.js
+++ utils/reports.js
@@ -287,7 +287,11 @@ async function createDayReport(billProducts, ingredients, loc, bills, dat, point
         }
     })
 
-
+    const ing = allIngs.find(i => i.name === 'Cafea True Blend')
+    if(ing){
+        console.log('First', ing.uploadLog[0])
+        console.log('Second', ing.uploadLog[ing.uploadLog.length - 1])
+    }
 
     for(const ing of allIngs){
         if(ing.uploadLog){
