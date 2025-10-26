c8d0885a32dda6b9070ab01554f7667f0718ff96
Alin Spinu <alinz.spinu@gmail.com>
2025-10-26 22:16:25 +0200

    all done

--- controlers/back-office/orders.js
+++ controlers/back-office/orders.js
@@ -297,7 +297,6 @@ module.exports.getOrderByUser = async (req, res, nex) => {
         const end = new Date(date).setHours(23, 59, 59, 999)
         const user = await User.findById(userId)
         const orders = await Order.find({locatie: user.locatie, 'employee.user': userId, status: 'done', createdAt: {$gte: start, $lt: end},  salePoint: point })
-        await modyfyOrdersProducts(orders)
         res.status(200).json(orders)
     } catch (err){
         console.log(err)
@@ -335,6 +334,7 @@ module.exports.getAllOrders = async (req, res, next) => {
         const start = new Date(date).setHours(0,0,0,0)
         const end = new Date(date).setHours(23, 59, 59, 999)
         const orders = await Order.find({locatie: loc, updatedAt: {$gte: start, $lt: end} , salePoint: point}).populate({path: 'masaRest', select: 'index name' })
+        await modyfyOrdersProducts(orders)
             res.status(200).json(orders)         
     } catch(err){
         console.log(err)
