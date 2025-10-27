60203038825896bc4aac5a7e771899739e0543a1
Alin Spinu <alinz.spinu@gmail.com>
2025-10-27 20:46:22 +0200

    all good

--- controlers/print/invoice.js
+++ controlers/print/invoice.js
@@ -61,8 +61,9 @@ doc.text(`${invoice.supplier.contact.email}`, 60 + 10, 120)
 doc.text(`${invoice.supplier.bank}`, 60 + 10, 132)
 doc.text(`${invoice.supplier.iban}`, 55 + 10, 144)
 
-
-doc.image('public/icons/logo-tzero.jpeg', 240, 50, {width: 80})
+if(invoice.supplier.name === 'NEW COLARA S.R.L.'){
+  doc.image('public/icons/logo-tzero.jpeg', 250, 50, {width: 80})
+}
 
 //HEADER CLIENT
 //Nume client
