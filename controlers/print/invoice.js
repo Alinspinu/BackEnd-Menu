d8ea5b263fc5ae1f7323320a2e6dd69553d2691a
Alin Spinu <alinz.spinu@gmail.com>
2025-10-29 14:12:19 +0200

    all good

--- controlers/print/invoice.js
+++ controlers/print/invoice.js
@@ -1,6 +1,8 @@
 const PDFDocument = require("pdfkit");
 
-const {round} = require('../../utils/functions')
+const {round} = require('../../utils/functions');
+const invoice = require("../../models/office/invoice");
+const { width } = require("pdfkit/js/page");
 
 function createInfoice(invoice){
 
@@ -352,7 +354,11 @@ function addIngredients(doc, ch, rowHeigth, y, page, headerHeight, rectHeigth, r
  doc.rect(25, 669, 338, 105)
  doc.lineWidth(0.5);
  doc.stroke()
-
+ if(invoice.note){
+   doc.fontSize(8)
+   doc.text('Informatii suplimentare:', 28, 672)
+   doc.text(invoice.note, 28, 682, {width: 300})
+ }
 
  doc.rect(363, 669, 197, 105)
  doc.lineWidth(0.5);
