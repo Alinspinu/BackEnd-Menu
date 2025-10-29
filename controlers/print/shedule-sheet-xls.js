31d0373777653ef5eeac6b4313dff37bbce2ff43
Alin Spinu <alinz.spinu@gmail.com>
2025-10-29 17:23:28 +0200

    all good

--- controlers/print/shedule-sheet-xls.js
+++ controlers/print/shedule-sheet-xls.js
@@ -35,8 +35,8 @@ const sheet = workbook.addWorksheet('Schedule');
 
 
   // ----- 1️⃣ BUILD MULTI-ROW HEADERS -----
-  const headerRow1 = ['User'];
-  const headerRow2 = [''];
+  const headerRow1 = [`${days[days.length - 1].date.toISOString().split('T')[0]}`];
+  const headerRow2 = [`${days[0].date.toISOString().split('T')[0]}`];
 
   days.forEach((d) => {
     const formatted = d.date.toISOString().split('T')[0] + ' ' + d.day; 
