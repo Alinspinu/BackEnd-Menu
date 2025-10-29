d8e71aae4a73f34019433066d85d6a3bd4b8d767
Alin Spinu <alinz.spinu@gmail.com>
2025-10-29 14:13:56 +0200

    all good

--- controlers/print/invoice.js
+++ controlers/print/invoice.js
@@ -354,11 +354,11 @@ function addIngredients(doc, ch, rowHeigth, y, page, headerHeight, rectHeigth, r
  doc.rect(25, 669, 338, 105)
  doc.lineWidth(0.5);
  doc.stroke()
-//  if(invoice.note){
-//    doc.fontSize(8)
-//    doc.text('Informatii suplimentare:', 28, 672)
-//    doc.text(invoice.note, 28, 682, {width: 300})
-//  }
+ if(invoice.note){
+   doc.fontSize(8)
+   doc.text('Informatii suplimentare:', 28, 672)
+   doc.text(invoice.note, 28, 682, {width: 300})
+ }
 
  doc.rect(363, 669, 197, 105)
  doc.lineWidth(0.5);
