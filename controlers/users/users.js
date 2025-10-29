79ffac386971fb2326ef068886da1d34e3f9192a
Alin Spinu <alinz.spinu@gmail.com>
2025-10-29 15:52:43 +0200

    all good

--- controlers/users/users.js
+++ controlers/users/users.js
@@ -16,19 +16,6 @@ const {hashPassword, encryptData} = require('../../utils/functions')
 
 
 
-module.exports.createUsersSheet = async (req, res) => {
-    const {start, end, loc, point} = req.body
-    try{
-        const users = await User.find({locatie: loc, client: false}, {selct: 'employee'})
-
-
-    } catch(e){
-        console.log(e)
-        res.status(500).json(e)
-    }
-}
-
-function filterLogs(user)
 
 
 module.exports.sendUsers = async (req, res, next) => {
