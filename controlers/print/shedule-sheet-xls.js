d93e3259eefeceef0881f239a4d4ab2818120dbd
Alin Spinu <alinz.spinu@gmail.com>
2025-10-29 17:35:11 +0200

    all good

--- controlers/print/shedule-sheet-xls.js
+++ controlers/print/shedule-sheet-xls.js
@@ -119,7 +119,7 @@ const sheet = workbook.addWorksheet('Condica de  prezenta');
     const cell = row.getCell(1);
     cell.border = {
       ...cell.border, // keep existing borders (if any)
-      right: { style: 'medium' }, // thicker line
+      right: { style: 'medium',color: { argb: 'FFCCCCCC' } }, // thicker line
     };
   });
 
@@ -129,7 +129,7 @@ const sheet = workbook.addWorksheet('Condica de  prezenta');
       const cell = row.getCell(rightColIndex);
       cell.border = {
         ...cell.border, // keep existing borders (if any)
-        right: { style: 'medium' }, // thicker line
+        right: { style: 'medium', color: { argb: 'FFCCCCCC' } }, // thicker line
       };
     });
   });
