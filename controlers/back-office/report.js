1fffa5e7c43e70c508dd92018974f5ad0c09cab4
Alin Spinu <alinz.spinu@gmail.com>
2025-10-31 09:22:53 +0200

    all done

--- controlers/back-office/report.js
+++ controlers/back-office/report.js
@@ -132,7 +132,8 @@ module.exports.getReportsDates = async (req, res) => {
 module.exports.getAllReports = async(req, res, next) => {
     try{
         const {loc, point, limit = 30} = req.query
-        const reports = await Report.find({locatie: loc, salePoint: point, status: 'new', period: { $exists: false }}).sort({day: -1})
+        const reports = await Report.find({locatie: loc, salePoint: point, status: 'new', period: { $exists: false }}).sort({day: -1}).lean()
+                // .select('departaments diverse day impairment ingsValue period profit')
         res.status(200).json(reports)
     } catch(err) {
         console.log(err)
