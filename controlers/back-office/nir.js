ec7cbdcaaaff8714ce255fb714b80faf6f241241
Alin Spinu <alinz.spinu@gmail.com>
2025-11-01 12:58:48 +0200

    all done

--- controlers/back-office/nir.js
+++ controlers/back-office/nir.js
@@ -256,6 +256,7 @@ module.exports.getNirs = async(req, res, next) => {
   try{
     const nirs = await Nir.find({locatie: loc, salePoint: point})
           .select('-ingredinets')
+          .limit(1000)
           .sort({ createdAt: -1 })
           .populate({path: 'suplier', select: '-records'}).lean()
           // .populate({path: 'ingredients.invGestiune', select: 'name'})
