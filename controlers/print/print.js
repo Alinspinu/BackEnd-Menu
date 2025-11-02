2be73302d04e6c2a81737898d361bd1f4b375745
Alin Spinu <alinz.spinu@gmail.com>
2025-11-02 15:18:03 +0200

    all done

--- controlers/print/print.js
+++ controlers/print/print.js
@@ -844,7 +844,7 @@ module.exports.printConsum = async (req, res) => {
                   productDep.total21 += total21
   
               } else {
-                const d = departaments.find(dep => dep._id.toString() === product.departament.toString())
+                const d = departaments.find(dep => dep.id.toString() === product.departament.toString())
                 if(d){
                   productDeps.push({
                     name: d.name,
