40d08ef742d28c9bd9737ffea92b8286ba1b947b
Alin Spinu <alinz.spinu@gmail.com>
2025-11-02 17:35:59 +0200

    all done

--- controlers/back-office/invoice/controlers.js
+++ controlers/back-office/invoice/controlers.js
@@ -104,6 +104,12 @@ module.exports.getInvoices = async (req, res) => {
   const {loc} = req.query
   try{
     const invoices = await Invoice.find({locatie: loc})
+    invoices[invoices.length -1].products.forEach(p => {
+      console.log(p.productId)
+    })
+    for(let i of invoices){
+      await i.save()
+    }
     res.status(200).json(invoices)
   } catch(error) {
     res.status(500).json(error)
