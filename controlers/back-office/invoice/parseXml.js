bb104c19d9389399e87b100b6c190e7d7c850952
Alin Spinu <alinz.spinu@gmail.com>
2025-11-03 18:55:35 +0200

    all done

--- controlers/back-office/invoice/parseXml.js
+++ controlers/back-office/invoice/parseXml.js
@@ -56,6 +56,7 @@ function parseInvoiceData(invoiceData, id) {
     const lineAmount = item?.LineExtensionAmount || {};
 
     const quantity = parseFloat(invoicedQuantity._ ?? invoicedQuantity) || 0;
+    console.log(invoicedQuantity)
     const unitCode = invoicedQuantity?.$?.unitCode || 'N/A';
     const price = parseFloat(priceData?.PriceAmount?._ ?? priceData?.PriceAmount) || 0;
     const totalNoVat = parseFloat(lineAmount._ ?? lineAmount) || 0;
