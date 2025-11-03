a569b2d04121707387760f952e43f66fbcb952a4
Alin Spinu <alinz.spinu@gmail.com>
2025-11-03 22:36:48 +0200

    all done

--- controlers/print/salary-pdf.js
+++ controlers/print/salary-pdf.js
@@ -110,7 +110,7 @@ function createSalaryReport(pontaj, mode, us){
 
       const pageWidth = doc.page.width;
 
-      let lineHeigth = 13
+      let lineHeigth = 14
       let y = 140
 
       doc.font("public/font/RobotoSlab-Regular.ttf")
@@ -152,8 +152,8 @@ function createSalaryReport(pontaj, mode, us){
             doc.text(`${ round(u.tax)}`, startLine+ 435, y + i * lineHeigth + lineHeigth);
             doc.lineWidth(0.1);
             doc
-            .moveTo(startLine, y + i * lineHeigth + lineHeigth + lineHeigth)
-            .lineTo(startLine + 470, y + i * lineHeigth + lineHeigth + lineHeigth)
+            .moveTo(startLine, y + i * lineHeigth + lineHeigth + lineHeigth + 2)
+            .lineTo(startLine + 470, y + i * lineHeigth + lineHeigth + lineHeigth + 2)
             .stroke();
         })
         let height = (users.length  * lineHeigth) + y;
