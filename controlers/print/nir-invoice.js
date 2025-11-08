284eed9bc6a4e66623948bd3fc49814c72ddb1bc
Alin Spinu <alinz.spinu@gmail.com>
2025-11-08 17:27:30 +0200

    all doner

--- controlers/print/nir-invoice.js
+++ controlers/print/nir-invoice.js
@@ -73,7 +73,7 @@ doc.text(`${invoice.locatie.account}`, 485  - 40, 90+rh , { width: 165, align: "
 // '+20'
 let yy = 130
 
-doc.roundedRect(185, yy + 20, 140 , 54, 1)
+doc.roundedRect(185, 140, yy + 40, 54, 1)
 doc.lineWidth(0.8);
 doc.stroke()
 // doc.font('Courier')
