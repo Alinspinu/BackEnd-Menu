d97554fa900f0a91b16b84342e52dc167bbad03a
Alin Spinu <alinz.spinu@gmail.com>
2025-11-08 21:14:46 +0200

    all doner

--- controlers/print/nir-invoice.js
+++ controlers/print/nir-invoice.js
@@ -53,7 +53,7 @@ doc.fontSize(10);
 // doc.font('Courier-Bold')
 doc.text('Client', 395 - 40, 50, { width: 35, align: "left" })
 doc.text(`C.I.F.`, 395 - 40, 62 +rh, { width: 30, align: "left" });
-doc.text(`Nr Reg.`, 395 - 40, 72 +rh, { width: 30, align: "left" });
+doc.text(`Nr Reg.`, 395 - 40, 74 +rh, { width: 40, align: "left" });
 doc.text(`Banca`, 395 - 40, 82 + rh + 5, { width: 35, align: "left" })
 doc.text(`Cont:`, 395 - 40, 100 + rh + 5, { width: 30, align: "left" })
 
@@ -66,7 +66,7 @@ doc.fontSize(10);
 doc.text(`${invoice.locatie.bussinessName}`, 395, 50, { width: 150, align: "left" });
 doc.font("public/font/Montserrat-Regular.ttf");
 doc.text(`${invoice.locatie.vatNumber}`, 395 , 62+rh , { width: 145, align: "left" });
-doc.text(`${invoice.locatie.register}`, 395 , 72+rh , { width: 145, align: "left" });
+doc.text(`${invoice.locatie.register}`, 395 , 74+rh , { width: 145, align: "left" });
 doc.text(`${invoice.locatie.bank}`, 395, 82+rh + 5 , { width: 165, align: "left" });
 doc.text(`${invoice.locatie.account}`, 395 , 100+rh + 5 , { width: 165, align: "left" });
 
