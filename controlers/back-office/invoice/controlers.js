a62768ac27b28031a9bc16512ef663c2d69f9207
Alin Spinu <alinz.spinu@gmail.com>
2025-11-14 15:46:56 +0200

    all done

--- controlers/back-office/invoice/controlers.js
+++ controlers/back-office/invoice/controlers.js
@@ -251,7 +251,7 @@ module.exports.getMessagesByDate = async (req, res) => {
     if(locatie){
       if(locatie.anafToken && locatie.anafToken.token){
         const cif = locatie.vatNumber.replace(/\D/g, '')
-        let page = 2
+        let page = 1
         const apiUrl1 = `https://api.anaf.ro/prod/FCTEL/rest/listaMesajePaginatieFactura?startTime=${startDate}&endTime=${endDate}&cif=${cif}&pagina=${page}&filtru${filter}`
         const config = {
           headers: {
