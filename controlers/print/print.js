6cac89872594382211060f931f5c60b2f489c7f8
Alin Spinu <alinz.spinu@gmail.com>
2025-11-15 20:30:14 +0200

    all done

--- controlers/print/print.js
+++ controlers/print/print.js
@@ -815,20 +815,22 @@ module.exports.printConsum = async (req, res) => {
           i.products.forEach(product => {
             product.productionCost = 0
             for(let i of product.ings){
-              if(i.ing.ings && i.ing.ings.length){
-                for(let ii of i.ing.ings){
-                  let price = ii.ing.price
-                  if(ii.ing.invGestiune[0].entries){
-                    price =  ii.ing.invGestiune[0]?.entries[0]?.priceNoVat || ii.ing.price
+              if(i.ing){
+                if(i.ing.ings && i.ing.ings.length){
+                  for(let ii of i.ing.ings){
+                    let price = ii.ing.price
+                    if(ii.ing.invGestiune[0].entries){
+                      price =  ii.ing.invGestiune[0]?.entries[0]?.priceNoVat || ii.ing.price
+                    }
+                    product.productionCost += (i.qty * ii.qty * price * product.quantity)
                   }
-                  product.productionCost += (i.qty * ii.qty * price * product.quantity)
+                } else {
+                  let price = i.ing.price
+                  if(i.ing.invGestiune[0].entries){
+                    price =  i.ing.invGestiune[0]?.entries[0]?.priceNoVat || i.ing.price
+                  }
+                  product.productionCost += (i.qty * price * product.quantity)
                 }
-              } else {
-                let price = i.ing.price
-                if(i.ing.invGestiune[0].entries){
-                  price =  i.ing.invGestiune[0]?.entries[0]?.priceNoVat || i.ing.price
-                }
-                product.productionCost += (i.qty * price * product.quantity)
               }
             }
             product.tot = product.total
@@ -857,6 +859,7 @@ module.exports.printConsum = async (req, res) => {
                     prod.quantity += product.quantity
                     prod.tot += product.tot
                     prod.discount += product.discount
+                    prod.productionCost += product.productionCost
                   } else {
                     productDep.products.push(product)
             
