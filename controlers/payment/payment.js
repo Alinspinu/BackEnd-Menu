c97c20455df8722b7b0227bf835bd2a6418adede
Alin Spinu <alinz.spinu@gmail.com>
2025-11-15 08:50:27 +0200

    all done

--- controlers/payment/payment.js
+++ controlers/payment/payment.js
@@ -370,8 +370,8 @@ module.exports.printBill = async (req, res, next) => {
             clientInfo: bill.clientInfo
         }
 
-        const digger = '690c818c21500095430c613f'
-        if(bill.locatie !== digger){
+        const digger = '690c818c21500095430c613f 655e2e7c5a3d53943c6b7c53'
+        if(!bill.locatie.includes(digger)){
             const locatie = await Locatie.findById(bill.locatie)
             if(locatie){
                 const bytes = await createBillForPrinter(bill, locatie.logoUrl || ' ', locatie.qrUrl)
