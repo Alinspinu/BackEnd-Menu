1aef2dfa6b8e1b94f016064c5a6c31f3d238211b
Alin Spinu <alinz.spinu@gmail.com>
2025-11-17 20:11:37 +0200

    all donr

--- controlers/back-office/orders.js
+++ controlers/back-office/orders.js
@@ -660,7 +660,8 @@ module.exports.prepStatusDone = async (req, res, next) => {
         if(!order){
             return res.status(404).json({message: 'No order was found'})
         }
-        
+        console.log(order)
+        console.log(id)
         for(let m of order.monitors){
             if(m.section.toString() === section){
                 m.prep = false
