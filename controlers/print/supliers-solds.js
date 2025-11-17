947e0f636adeed40073f69c875be5577bddc18b2
Alin Spinu <alinz.spinu@gmail.com>
2025-11-17 17:31:33 +0200

    all donr

--- controlers/print/supliers-solds.js
+++ controlers/print/supliers-solds.js
@@ -7,7 +7,7 @@ function createSupliersSolds(supliers, doc){
 
     let height = 30
 
-    doc.image('public/icons/logo_true.png', 200, height, {width: 80})
+    doc.image('public/icons/logo_true.png', 180, height, {width: 80})
 
     height += 60
 
@@ -15,24 +15,24 @@ function createSupliersSolds(supliers, doc){
 
     doc.fontSize(14)
     doc.font('public/font/Montserrat-Bold.ttf')
-    doc.text('Situatie furnizori', 220, height)
+    doc.text('Situatie furnizori', 160, height)
 
     height += 20
 
     doc.fontSize(12)
     doc.font("public/font/Montserrat-Regular.ttf");
    supliers.forEach((s, i) => {
-       doc.text(`${i+1}`, 180, height)
-       doc.text(`${s.name}`, 195, height)
-       doc.text(`${s.sold} Lei`, 355, height)
+       doc.text(`${i+1}`, 40, height)
+       doc.text(`${s.name}`, 60, height)
+       doc.text(`${s.sold} Lei`, 300, height)
        height+=13
        total += s.sold
    })
 
    doc.fontSize(14)
    doc.font('public/font/Montserrat-Bold.ttf')
-   doc.text('TOTAL', 195, height)
-   doc.text(`${round(total)}`, 345, height)
+   doc.text('TOTAL', 60, height)
+   doc.text(`${round(total)}`, 290, height)
 
 
 return doc
