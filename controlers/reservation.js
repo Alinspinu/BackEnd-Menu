a98b37ebad4bea2133f2b8888fd1c362da6c2e58
Alin Spinu <alinz.spinu@gmail.com>
2025-11-18 18:31:07 +0200

    all done

--- controlers/reservation.js
+++ controlers/reservation.js
@@ -50,7 +50,7 @@ module.exports.getReservationShedule = async (req, res) => {
 module.exports.updateReservationShedule = async (req, res) => {
     const {shedule} = req.body
     try{
-        const updatedShedule = await ReservationSchedule.findByIdAndUpdate(SyntheticModule._id, shedule, {new: true})
+        const updatedShedule = await ReservationSchedule.findByIdAndUpdate(shedule._id, shedule, {new: true})
         res.status(200).json({shedule: updatedShedule, message: 'Modificarile au fost efectuate!'})
 
     } catch(error){
