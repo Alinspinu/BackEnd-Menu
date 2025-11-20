3b158e7b0c9ccf64ac8ff6d672656f678ee80a24
Alin Spinu <alinz.spinu@gmail.com>
2025-11-20 20:17:59 +0200

    all done

--- controlers/users/users.js
+++ controlers/users/users.js
@@ -52,6 +52,7 @@ module.exports.sendEmployees = async (req, res) => {
         const user = await User.find({locatie: loc, client: false, 'employee.active': true}).select('-password').populate({path: 'employee.employeePosition'});
         const sortedUsers = user.sort((a, b) => a.name.localeCompare(b.name));
         // await modifyUsers(sortedUsers)
+        console.log(sortedUsers)
         res.status(200).json(sortedUsers)
     } catch(err) {
         console.log(err)
