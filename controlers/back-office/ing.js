ca6a29045124b7d3e1b7c086f3a1885cc0178466
Alin Spinu <alinz.spinu@gmail.com>
2025-11-20 08:25:42 +0200

    all done

--- controlers/back-office/ing.js
+++ controlers/back-office/ing.js
@@ -111,7 +111,7 @@ module.exports.saveIng = async(req, res, next) => {
           .populate({path: 'eFactura.gestiune', select: 'name'})
         const totalItems = 1500
         const totalPages = Math.ceil(totalItems / limit);
-        verifyIngredients(items)
+        // verifyIngredients(items)
         res.status(200).json({
           items,
           totalPages,
