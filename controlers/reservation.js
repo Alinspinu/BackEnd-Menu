04a1a49e301167608084211fc320d6c1b110f58b
Alin Spinu <alinz.spinu@gmail.com>
2025-11-22 13:50:00 +0200

    all done

--- controlers/reservation.js
+++ controlers/reservation.js
@@ -38,6 +38,7 @@ module.exports.getReservationShedule = async (req, res) => {
     try{
        const y = new Date(year)
         const shedule = await ReservationSchedule.findOne({locatie: loc, salePoint: point, 'year.date': y})
+                    .populate({path: 'year.months', populate: {path: 'days', populate: {path: 'hours'}}})
 
         res.status(200).json(shedule)
 
