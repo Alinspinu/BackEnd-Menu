60064490f39ec9868e940adc35acdb8437c1dd21
Alin Spinu <alinz.spinu@gmail.com>
2025-11-22 15:10:04 +0200

    all done

--- controlers/reservation.js
+++ controlers/reservation.js
@@ -64,7 +64,9 @@ module.exports.updateSheduleHours = async (req, res) => {
               );
         }
         Promise.all(updates)
-        .then(() => ReservationSchedule.findById(sheduleId).populate({path: 'year.months', populate: {path: 'days', populate: {path: 'hours'}}}).lean())
+        .then(() => {
+            return  ReservationSchedule.findById(sheduleId).populate({path: 'year.months', populate: {path: 'days', populate: {path: 'hours'}}}).lean()
+        })
         .then(newShedule => {
           res.status(200).json({
             shedule: newShedule,
@@ -111,7 +113,9 @@ module.exports.updateReservationSheduleSettings = (req, res) => {
   
     // run all updates in parallel
     Promise.all(updates)
-      .then(() => ReservationSchedule.findById(shedule._id).populate({path: 'year.months', populate: {path: 'days', populate: {path: 'hours'}}}).lean())
+        .then(() => {
+            return  ReservationSchedule.findById(shedule._id).populate({path: 'year.months', populate: {path: 'days', populate: {path: 'hours'}}}).lean()
+        })
       .then(newShedule => {
         res.status(200).json({
           shedule: newShedule,
