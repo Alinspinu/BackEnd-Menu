c0b153c962cd32a00237aa70d8eccc446d1cca27
Alin Spinu <alinz.spinu@gmail.com>
2025-11-24 00:16:57 +0200

    all done

--- controlers/reservation.js
+++ controlers/reservation.js
@@ -54,12 +54,17 @@ module.exports.getReservationShedule = async (req, res) => {
         res.status(500).json(error)
     }
 }
+
+
 module.exports.getReservationSheduleById = async (req, res) => {
     const {id} = req.query
+    console.log(id)
     try{
         const shedule = await ReservationSchedule.findById(id)
                     .populate({path: 'year.months', populate: {path: 'days', populate: {path: 'hours', populate: {path: 'reservations'}}}}).lean()
 
+
+        console.log(shedule)
         res.status(200).json(shedule)
 
     } catch(error){
