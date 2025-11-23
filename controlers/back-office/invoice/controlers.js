0c07167af735b1c077474dafceead93c41670849
Alin Spinu <alinz.spinu@gmail.com>
2025-11-23 12:17:22 +0200

    all done

--- controlers/back-office/invoice/controlers.js
+++ controlers/back-office/invoice/controlers.js
@@ -71,7 +71,7 @@ module.exports.uploadInvoiceToEFactura = async (req, res) => {
       return res.status(404).json({message: 'Missing token'})
     }
     const xml = invoice.invoice ? buildEFacturaHeaderXML(invoice) : buildEFacturaHeaderXML(invoice, invoice.issueDate)
-    // console.log(xml)
+    console.log(xml)
     let vatNumber = invoice.supplier.vatNumber.replace(/\D/g, '');
     const response = await uploadInvoice(xml, vatNumber, false, token)
     invoice.eFacturaId = response.eFacturaId
