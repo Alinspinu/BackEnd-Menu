192362f81d9623e7ab9457c31ba0a85b562b8f30
Alin Spinu <alinz.spinu@gmail.com>
2025-11-30 19:58:33 +0200

    all done

--- controlers/back-office/nir.js
+++ controlers/back-office/nir.js
@@ -92,7 +92,7 @@ module.exports.createSheetByOrder = async (req, res) => {
     for(let p of order.products){
 
       for(let i of p.ings){
-        i.qty = i.qty * product.quantity
+        i.qty = i.qty * p.quantity
         if(i.ing.productIngredient){
           for(let ii of i.ing.ings){
              ii.qty = ii.qty * i.qty
