301cd4ec7472dddee8f1ed4f730f69b396c03877
Alin Spinu <alinz.spinu@gmail.com>
2025-11-30 03:34:57 +0200

    all done

--- controlers/back-office/ing.js
+++ controlers/back-office/ing.js
@@ -111,7 +111,7 @@ module.exports.saveIng = async(req, res, next) => {
           .populate({path: 'eFactura.gestiune', select: 'name'})
         const totalItems = 1500
         const totalPages = Math.ceil(totalItems / limit);
-        await  verifyIngredients(items)
+        // await  verifyIngredients(items)
         res.status(200).json({
           items,
           totalPages,
@@ -133,7 +133,7 @@ module.exports.saveIng = async(req, res, next) => {
           if(i.invGestiune.length){
             if(i.invGestiune[0].entries.length){
               if(i.invGestiune[0].entries[0].priceNoVat <= 0){
-                console.log(i.name, 'pret ing ', i.price)
+                // console.log(i.name, 'pret ing ', i.price)
                 i.invGestiune[0].entries[0].priceNoVat = i.price
                 i.invGestiune[0].entries[0].priceWithVat = round(i.price * (1 + (i.tva/100)))
                 ingsToUpdate.push(i)
