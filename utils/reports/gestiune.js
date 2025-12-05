d78d6f3e061013a3074e22a6b06d97acc4ce9680
Alin Spinu <alinz.spinu@gmail.com>
2025-12-05 12:31:02 +0100

    all done

--- utils/reports/gestiune.js
+++ utils/reports/gestiune.js
@@ -161,10 +161,10 @@ async function createRG(start, end, nirs, ings, gest, orders, in0, in11, in21, d
 
                     if(p.tva === 11){
                       totalOut11 += price
-                      console.log('prduse cu tva 11 la data ', formatedDateToShow(o.createdAt), p.name, " ingredient ", p.ings)
                     }
                     if(p.tva === 21){
                       totalOut21 += price
+                      console.log('prduse cu tva 21 la data ', formatedDateToShow(o.createdAt), p.name, " ingredient ", p.ings)
                     }
 
                     const existingEntry = day.entries.find(e => e.description === 'Vanzare cu amanuntul' && e.tva === p.tva)
