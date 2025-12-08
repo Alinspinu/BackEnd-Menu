9c9bd0e46754ea800671227faf159f7115c0b400
Alin Spinu <alinz.spinu@gmail.com>
2025-12-08 15:58:00 +0100

    all done

--- controlers/payment/payment.js
+++ controlers/payment/payment.js
@@ -348,16 +348,18 @@ module.exports.printBill = async (req, res, next) => {
             }
             await client.save()
         }
-        update = {
-            status: 'done',
-            pending: false,
-            payment: bill.payment,
-            paymentMethod: bill.paymentMethod,
-            tips: bill.tips,
-            total: bill.total,
-            clientInfo: bill.clientInfo,
-            createdAt: new Date()
-        }
+        const update = {
+            $set: {
+              status: 'done',
+              pending: false,
+              payment: bill.payment,
+              paymentMethod: bill.paymentMethod,
+              tips: bill.tips,
+              total: bill.total,
+              clientInfo: bill.clientInfo,
+              createdAt: new Date()
+            }
+          };
 
         const digger = ['690c818c21500095430c613f', '655e2e7c5a3d53943c6b7c53']
         if(!digger.includes(bill.locatie)){
