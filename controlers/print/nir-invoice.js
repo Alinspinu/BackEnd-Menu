8f50b731cf24811e81b68f7694b14a46f5df57a9
Alin Spinu <alinz.spinu@gmail.com>
2025-12-09 13:42:12 +0100

    all done

--- controlers/print/nir-invoice.js
+++ controlers/print/nir-invoice.js
@@ -349,7 +349,7 @@ function addIngredients(doc, ch, rowHeigth, y, page, headerHeight, rectHeigth, r
           doc.fontSize(9)
         }
         heghtValue += rowHeigth
-        if(name.length > 40){
+        if(name.length > 42){
           heghtValue += 6
         }
         if(name.length > 70){
