1f127e7a7d0ff8ed8db836581025126918bebb49
Alin Spinu <alinz.spinu@gmail.com>
2025-12-12 17:19:47 +0100

    all done

--- controlers/back-office/invoice/controlers.js
+++ controlers/back-office/invoice/controlers.js
@@ -93,13 +93,13 @@ module.exports.uploadInvoiceToEFactura = async (req, res) => {
     const xml = invoice.invoice ? buildEFacturaHeaderXML(invoice) : buildEFacturaHeaderXML(invoice, invoice.issueDate)
     console.log(xml)
     let vatNumber = invoice.supplier.vatNumber.replace(/\D/g, '');
-    // const response = await uploadInvoice(xml, vatNumber, false, token)
-    // invoice.eFacturaId = response.eFacturaId
-    // invoice.eFacturaError = response.eFacturaError
-    // invoice.eFacturaStatus = response.eFacturaStatus
-    // const savedInvoice = await Invoice.findByIdAndUpdate(id, invoice, {new: true})
-    // res.status(200).json({message: response.message, invoice: savedInvoice})
-    res.status(200).json({message: ''})
+    const response = await uploadInvoice(xml, vatNumber, false, token)
+    invoice.eFacturaId = response.eFacturaId
+    invoice.eFacturaError = response.eFacturaError
+    invoice.eFacturaStatus = response.eFacturaStatus
+    const savedInvoice = await Invoice.findByIdAndUpdate(id, invoice, {new: true})
+    res.status(200).json({message: response.message, invoice: savedInvoice})
+    // res.status(200).json({message: ''})
   } catch(error){
     console.log(error)
     res.status(500).json(error)
