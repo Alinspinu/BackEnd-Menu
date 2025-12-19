03b447a54fb5bafdd6c3446cb292eb4211d90866
Alin Spinu <alinz.spinu@gmail.com>
2025-12-19 17:37:53 +0200

    all done

--- controlers/users/auth.js
+++ controlers/users/auth.js
@@ -383,7 +383,7 @@ module.exports.login = async (req, res, next) => {
                             })
 
          if(users.length > 1 && !loc){
-            const locs = users.map(u =>{ return {name: u.locatie.name, id: u.locatie._id}})
+            const locs = users.map(u =>{ return {name: u.locatie?.name, id: u.locatie._id}})
             return res.status(200).json({message: 'Acest email este folosit în mai multe locații! Alege la ce locație vrei să te conectezi!', locs: locs, multiple: true})
          } 
          if(users.length === 1 || Array.isArray(loc)){
