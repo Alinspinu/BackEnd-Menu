ee4e26481917466c13fe8e602d2842872394d876
Alin Spinu <alinz.spinu@gmail.com>
2026-01-06 12:03:57 +0200

    all done

--- controlers/reservation.js
+++ controlers/reservation.js
@@ -298,7 +298,7 @@ module.exports.updateReservationSheduleSettings = (req, res) => {
     const {loc, point, year} = req.query
     try{
        const y = new Date(year)
-        const shedule = await ReservationSchedule.findOne({locatie: loc, salePoint: point, temp: true, 'year.date': y})
+        const shedule = await ReservationSchedule.findOne({locatie: loc, salePoint: point, temp: false, 'year.date': y})
                     .populate({path: 'year.months', populate: {path: 'days', populate: {path: 'hours', populate: {path: 'reservations'}}}}).lean()
 
         const sh = await updateReservationShedule(shedule)
