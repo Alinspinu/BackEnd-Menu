c64dd2b9b4963d355d000bdf529a1c5dd6eebbf7
Alin Spinu <alinz.spinu@gmail.com>
2026-01-08 09:47:56 +0200

    all done

--- controlers/back-office/invoice/controlers.js
+++ controlers/back-office/invoice/controlers.js
@@ -151,7 +151,7 @@ module.exports.getInvoices = async (req, res) => {
     for(let i of invoices){
       for(let p of i.products){
         if(p.productId?.toString() === "64c3bc7fbae1d3ed496dd925"){
-          console.log(p.name, p.quantity)
+          console.log(p.name, p.quantity, ' - ', i.issueDate)
         }
       }
 
