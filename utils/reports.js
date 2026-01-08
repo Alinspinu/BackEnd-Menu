1c1ed0104a2acb27a8ec3282a927ecd8aea77441
Alin Spinu <alinz.spinu@gmail.com>
2026-01-08 10:37:25 +0200

    all done

--- utils/reports.js
+++ utils/reports.js
@@ -356,10 +356,6 @@ async function createDayReport(billProducts, ingredients, loc, bills, dat, point
     for(let i of invoices){
         console.log('produse factura ', i.products.length)
         for(let p of i.products){
-            if(p.name === "Croissant cu Scrob, Trufe și Ciuperci"){
-                console.log(p.name, p.quantity, ' - ', i.issueDate)
-                console.log(typeof(p.quantity))
-              }
             values.totalIngredients += calacProductRecipe(p)
             const g = productsGest.find(pg => pg.name === p.productId?.gestiune?.name)
             if(g){
@@ -429,80 +425,76 @@ async function createDayReport(billProducts, ingredients, loc, bills, dat, point
     
 
   
-    // for(let prod of billProducts){ 
-    //     values.totalIngredients += calacProductRecipe(prod)
-    //     if(prod.productId){
-    //         if(prod.name === "Croissant cu Scrob, Trufe și Ciuperci"){
-    //             console.log(prod.name, prod.quantity, ' - ')
-    //             console.log(typeof(prod.quantity))
-    //           }
-    //         if(prod.productId.gestiune){
-    //             const g = productsGest.find(pg => pg.name === prod.productId.gestiune.name)
-    //             if(g){
-    //                 const price = (prod.price*prod.quantity) - prod.discount
-    //                 totall += price
-    //                 const totalRecipe = calacProductRecipe(prod)
-    //                 const existingProduct = g.products.find(p => p.name === prod.name)
-    //                 if(existingProduct){
-    //                     existingProduct.qty = existingProduct.qty + prod.quantity
-    //                     existingProduct.price = round(existingProduct.price + price)
-    //                     existingProduct.totalRecipe = round(existingProduct.totalRecipe + totalRecipe)
-    //                     g.totalOut += price
-    //                     const existingDep= g.dep.find(d => (d.name === prod.productId.departament.name))
-    //                     if(existingDep){
-    //                         existingDep.totalRecipes += totalRecipe
-    //                         existingDep.totalOut = existingDep.totalOut + round(price)
-    //                     } else {
-    //                         g.dep.push(
-    //                             {
-    //                                 name: prod.productId.departament.name,
-    //                                 depId: prod.ings.length  && prod.ings[0].ing.dept ? prod.ings[0].ing.dept.toString() : prod.productId.departament._id.toString(),
-    //                                 totalOut: price,
-    //                                 totalIn: 0,
-    //                                 totalInvIn: 0,
-    //                                 totalInvOut: 0,
-    //                                 totalRecipes: totalRecipe,
-    //                             }
-    //                             )
-    //                         }
-    //                     } else {
-    //                       const product = {
-    //                             name: prod.name,
-    //                             dep: prod.productId.departament.name,
-    //                             depId: prod.productId.departament._id,
-    //                             qty: prod.quantity,
-    //                             price: price,
-    //                             totalRecipe: totalRecipe
-    //                         }
-    //                         g.totalOut += price
-    //                         g.products.push(product)
-    //                         const existingDep = g.dep.find(p => (p.name === prod.productId.departament.name))
-    //                         if(existingDep) {
-    //                             existingDep.totalRecipes += totalRecipe
-    //                             existingDep.totalOut += price
-    //                         } else {
-    //                             g.dep.push(
-    //                                 {
-    //                                     name: prod.productId.departament.name,
-    //                                     totalOut: price,
-    //                                     depId: prod.ings.length && prod.ings[0].ing.dept ? prod.ings[0].ing.dept.toString() : prod.productId.departament._id.toString(),
-    //                                     totalIn: 0,
-    //                                     totalInvIn: 0,
-    //                                     totalInvOut: 0,
-    //                                     totalRecipes: totalRecipe,
-    //                                 }
-    //                             )
-    //                         }
-    //                     }
-    //                 }
-    //         } else {
-    //             // console.log('produs fara gestiune', prod.name)
-    //         }
-    //     } else {
-    //         // console.log('produs fara prodictId', prod.name)
-    //     }
+    for(let prod of billProducts){ 
+        values.totalIngredients += calacProductRecipe(prod)
+        if(prod.productId){
+            if(prod.productId.gestiune){
+                const g = productsGest.find(pg => pg.name === prod.productId.gestiune.name)
+                if(g){
+                    const price = (prod.price*prod.quantity) - prod.discount
+                    totall += price
+                    const totalRecipe = calacProductRecipe(prod)
+                    const existingProduct = g.products.find(p => p.name === prod.name)
+                    if(existingProduct){
+                        existingProduct.qty = existingProduct.qty + prod.quantity
+                        existingProduct.price = round(existingProduct.price + price)
+                        existingProduct.totalRecipe = round(existingProduct.totalRecipe + totalRecipe)
+                        g.totalOut += price
+                        const existingDep= g.dep.find(d => (d.name === prod.productId.departament.name))
+                        if(existingDep){
+                            existingDep.totalRecipes += totalRecipe
+                            existingDep.totalOut = existingDep.totalOut + round(price)
+                        } else {
+                            g.dep.push(
+                                {
+                                    name: prod.productId.departament.name,
+                                    depId: prod.ings.length  && prod.ings[0].ing.dept ? prod.ings[0].ing.dept.toString() : prod.productId.departament._id.toString(),
+                                    totalOut: price,
+                                    totalIn: 0,
+                                    totalInvIn: 0,
+                                    totalInvOut: 0,
+                                    totalRecipes: totalRecipe,
+                                }
+                                )
+                            }
+                        } else {
+                          const product = {
+                                name: prod.name,
+                                dep: prod.productId.departament.name,
+                                depId: prod.productId.departament._id,
+                                qty: prod.quantity,
+                                price: price,
+                                totalRecipe: totalRecipe
+                            }
+                            g.totalOut += price
+                            g.products.push(product)
+                            const existingDep = g.dep.find(p => (p.name === prod.productId.departament.name))
+                            if(existingDep) {
+                                existingDep.totalRecipes += totalRecipe
+                                existingDep.totalOut += price
+                            } else {
+                                g.dep.push(
+                                    {
+                                        name: prod.productId.departament.name,
+                                        totalOut: price,
+                                        depId: prod.ings.length && prod.ings[0].ing.dept ? prod.ings[0].ing.dept.toString() : prod.productId.departament._id.toString(),
+                                        totalIn: 0,
+                                        totalInvIn: 0,
+                                        totalInvOut: 0,
+                                        totalRecipes: totalRecipe,
+                                    }
+                                )
+                            }
+                        }
+                    }
+            } else {
+                console.log('produs fara gestiune', prod.name)
+            }
+        } else {
+            console.log('produs fara prodictId', prod.name)
+        }
 
-    // }
+    }
 
 
 
