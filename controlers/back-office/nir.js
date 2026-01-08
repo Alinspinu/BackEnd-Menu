aa84d98a4947655c3a77a1e170b7734cd3093aee
Alin Spinu <alinz.spinu@gmail.com>
2026-01-08 17:13:29 +0200

    all done

--- controlers/back-office/nir.js
+++ controlers/back-office/nir.js
@@ -521,7 +521,7 @@ module.exports.printSheets = async (req, res) => {
     res.send(Buffer.from(buffer));
 
   } catch(error){
-    consol.log(error)
+    consolr.log(error)
     res.status(200).json(error)
   }
 } 
