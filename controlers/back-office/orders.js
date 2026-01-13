010897bcb5d281ca3423d1b3968b1f9322bde6cc
Alin Spinu <alinz.spinu@gmail.com>
2026-01-13 14:52:14 +0200

    all done

--- controlers/back-office/orders.js
+++ controlers/back-office/orders.js
@@ -523,8 +523,9 @@ module.exports.saveOrEditBill = async (req, res, next) => {
     const {bill, mode, mainServer, secondaryServer} = req.body;
     const parsedBill = JSON.parse(bill)
     const {index, billId} = req.query;
-    const table = await Table.findOne({index: index, locatie: parsedBill.locatie, salePoint: parsedBill.salePoint})
-    // const table = await Table.findById(parsedBill.masaRest)
+    // const table = await Table.findOne({index: index, locatie: parsedBill.locatie, salePoint: parsedBill.salePoint})
+    const table = await Table.findById(parsedBill.masaRest)
+    console.log(table)
     try{
         if(billId === "new"){
             delete parsedBill._id
