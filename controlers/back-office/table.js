063383d9f1ea7f4648862da59e424a892701500c
Alin Spinu <alinz.spinu@gmail.com>
2026-01-13 16:58:18 +0200

    all done

--- controlers/back-office/table.js
+++ controlers/back-office/table.js
@@ -106,7 +106,8 @@ module.exports.deleteTable = async (req, res, next) => {
 
 
 module.exports.getArea = async (req, res) => {
-    const {loc, point} =  req.body
+    const {loc, point} =  req.query
+    consoel.log(loc, point)
     try{
         const areas = await Area.find({locatie: loc, salePoint: point})
                     .populate({path: 'tables', 
@@ -117,7 +118,7 @@ module.exports.getArea = async (req, res) => {
                             populate: {path: 'masaRest', select: 'index'}
                         }
                     })
-    
+    console.log(areas)
      res.status(200).json(areas)
     } catch(error){
         console.log(error)
