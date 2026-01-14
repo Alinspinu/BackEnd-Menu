6beae69756a704b2f6d6d1a95dc58d1f8e8591b4
Alin Spinu <alinz.spinu@gmail.com>
2026-01-14 21:04:34 +0200

    all done

--- controlers/back-office/orders.js
+++ controlers/back-office/orders.js
@@ -56,7 +56,7 @@ module.exports.getOrder = async (req, res, next) => {
                         .populate({path: 'products.gestiune', select: 'name'})
                         .populate({path: 'products.departament', select: 'name'})
                         // .populate({path: 'products.productId', select: 'name departament'})
-                        .populate({path: 'products.productId', select: 'name ings subProducts', populate: {path: 'subProducts', select: 'name ings'}}).lean()
+                        // .populate({path: 'products.productId', select: 'name ings subProducts', populate: {path: 'subProducts', select: 'name ings'}}).lean()
                         .lean()
         const openOrders = await Order.find({ locatie: loc, status: 'open', salePoint: point})
                         .populate({path: 'masaRest', select: 'name index'})
@@ -64,7 +64,7 @@ module.exports.getOrder = async (req, res, next) => {
                         .populate({path : 'products.departament', select: 'name'}).lean()
                         // .populate({path: 'products.productId', select: 'departament'})
         const delProds = await DelProd.find({locatie: loc, createdAt: {$gte: startTime, $lt: endTime}, salePoint: point}).lean()
-        await modifyOrdersProducts(orders)
+        // await modifyOrdersProducts(orders)
         if(download && download.bool){
             const date = `${formatedDateToShow(start).split('ora')[0]} - ${formatedDateToShow(end).split('ora')[0]}`
             let buffer
