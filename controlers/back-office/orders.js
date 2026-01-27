a29fef5b2dea98f9841fd6cfebcc1a05a32190e4
Alin Spinu <alinz.spinu@gmail.com>
2026-01-27 03:27:58 +0200

    all done

--- controlers/back-office/orders.js
+++ controlers/back-office/orders.js
@@ -792,7 +792,7 @@ module.exports.checkCartProducts = async (req, res) => {
                 if(sub){
                    if(sub.stock.active && sub.stock.value - p.quantity < 1){
                      status = false
-                     message += ' ' + p.name + ' cantitate insuficienta în stoc ' + sub.stock.value -1 + " buc!"
+                     message += ' ' + p.name + ' cantitate insuficienta în stoc ' + `${sub.stock.value - 1 } buc!`
                      productNames.push(p.name)
                    } 
                 } else {
@@ -805,7 +805,7 @@ module.exports.checkCartProducts = async (req, res) => {
                 if(prod){
                     if(prod.stock.active && prod.stock.value - p.quantity < 1){
                         status = false
-                        message +=  ' ' + p.name + ' cantitate insuficienta în stoc ' + prod.stock.value -1 + " buc!"
+                        message +=  ' ' + p.name + ' cantitate insuficienta în stoc ' + `${prod.stock.value - 1 } buc!`
                         productNames.push(p.name)
                       } 
 
